d2f7f8f107717c2f3b8ebde45f593624b6385a24
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-28 12:39:48 -0500

    Adapted drawing component tests to use tool-selector service rather than local attribute currentTool

--- client/src/app/components/drawing/drawing.component.spec.ts
+++ client/src/app/components/drawing/drawing.component.spec.ts
@@ -1,9 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Tool } from '@app/classes/tool';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 import { PencilService } from '@app/services/tools/pencil-service';
 import { DrawingComponent } from './drawing.component';
+
 
 class ToolStub extends Tool {}
 
@@ -48,7 +48,7 @@ describe('DrawingComponent', () => {
     });
 
     it('should get stubTool', () => {
-        const currentTool = component.currentTool;
+        const currentTool = component['toolSelector'].getSelectedTool();
         expect(currentTool).toEqual(toolStub);
     });
 
