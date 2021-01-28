3b99f6104773d3282ff3aa384201fa977c233fb7
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-28 12:37:30 -0500

    Added test normal path for selectTool method in tool-selector service

--- client/src/app/services/tool-selector/tool-selector.service.spec.ts
+++ client/src/app/services/tool-selector/tool-selector.service.spec.ts
@@ -1,16 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
+import { PencilService } from '../tools/pencil-service';
 import { ToolSelectorService } from './tool-selector.service';
 
+
 describe('ToolSelectorService', () => {
-  let service: ToolSelectorService;
+    let service: ToolSelectorService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ToolSelectorService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(ToolSelectorService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should change tool to pencil when selectTool(\'pencil\') is called', () => {
+        service.selectTool('pencil');
+        expect(service.getSelectedTool() instanceof PencilService).toBeTruthy();
+    });
 });
