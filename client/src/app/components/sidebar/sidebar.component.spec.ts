b72ecd7a3c82b16e79af42f372080a4aa2a04ded
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-29 14:09:32 -0500

    Added tests for tool selection from sidebar

--- client/src/app/components/sidebar/sidebar.component.spec.ts
+++ client/src/app/components/sidebar/sidebar.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SidebarComponent } from './sidebar.component';
 
+// tslint:disable:no-any
 describe('SidebarComponent', () => {
     let component: SidebarComponent;
     let fixture: ComponentFixture<SidebarComponent>;
@@ -20,5 +20,23 @@ describe('SidebarComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('should set selectedToolName to new toolName when calling selectTool', () => {
+        const toolName = 'pencil';
+        component.selectTool('pencil');
+        expect(component.selectedToolName).toBe(toolName);
+    });
+
+    it('should return the display name of a tool when getDisplayName is called with a valid tool name', () => {
+        const expectedDisplayName = 'Crayon';
+        const obtainedDisplayName: string = component.getDisplayName('pencil');
+        expect(obtainedDisplayName).toBe(expectedDisplayName);
+    });
+
+    it("should return '<Outil inconnu>' when getDisplayName is called with an invalid tool name", () => {
+        const expectedDisplayName = '<Outil inconnu>';
+        const obtainedDisplayName: string = component.getDisplayName('invalid tool');
+        expect(obtainedDisplayName).toBe(expectedDisplayName);
     });
 });
