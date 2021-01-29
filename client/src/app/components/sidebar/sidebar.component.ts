3e1abe5109f72e354da49fe4e2fe479dc3389cfd
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-29 14:05:32 -0500

    Injected tool-selector service in sidebar component and added proxy methods for it

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { ToolSelectorService } from '@app/services/tool-selector/tool-selector.service';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+    selectedToolName: string = 'pencil';
+
+    toolNames: string[];
+
+    selectTool(toolName: string): void {
+        this.selectedToolName = toolName;
+        this.toolSelectorService.selectTool(toolName);
+    }
+
+    getDisplayName(toolName: string): string {
+        const displayName = this.toolSelectorService.getDisplayName(toolName);
+        return displayName === undefined ? '<Outil inconnu>' : displayName;
+    }
+
+    constructor(private toolSelectorService: ToolSelectorService) {
+        this.toolNames = Array.from(this.toolSelectorService.getRegisteredTools().keys());
+    }
+}
