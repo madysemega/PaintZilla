4ad3df9c97c09e73d9131f923dbbf4ec058fc6a4
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-29 14:12:42 -0500

    Imported button, divier, icon, sidenav and tooltip modules from angular material into app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -11,7 +16,17 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
     declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent],
-    imports: [BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatIconModule,
+        MatTooltipModule,
+        MatDividerModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
