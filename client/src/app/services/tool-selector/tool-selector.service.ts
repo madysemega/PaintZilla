be57ea967ee6d42a110f5f4d31e376547722f769
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-29 14:02:42 -0500

    Ajusted tool-selector service to use named tools rather than unnamed ones

--- client/src/app/services/tool-selector/tool-selector.service.ts
+++ client/src/app/services/tool-selector/tool-selector.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { NamedTool } from '@app/classes/named-tool';
 import { Tool } from '@app/classes/tool';
 import { EllipseService } from '@app/services/tools/ellipse-service.service';
 import { PencilService } from '@app/services/tools/pencil-service';
@@ -7,25 +8,29 @@ import { PencilService } from '@app/services/tools/pencil-service';
     providedIn: 'root',
 })
 export class ToolSelectorService {
-    private tools: Map<string, Tool> = new Map<string, Tool>();
-    private selectedTool: Tool;
+    private tools: Map<string, NamedTool> = new Map<string, NamedTool>();
+    private selectedTool: NamedTool;
 
     getSelectedTool(): Tool {
-        return this.selectedTool;
+        return this.selectedTool.tool;
     }
 
     selectTool(toolName: string): void {
         if (this.tools.has(toolName)) {
-            this.selectedTool = this.tools.get(toolName) as Tool;
+            this.selectedTool = this.tools.get(toolName) as NamedTool;
         }
     }
 
-    getRegisteredTools(): Map<string, Tool> {
+    getRegisteredTools(): Map<string, NamedTool> {
         return this.tools;
     }
 
+    getDisplayName(toolName: string): string | undefined {
+        return this.tools.get(toolName)?.name;
+    }
+
     constructor(pencilService: PencilService, ellipseService: EllipseService) {
-        this.tools.set('pencil', pencilService);
-        this.tools.set('ellipse', ellipseService);
+        this.tools.set('pencil', { name: 'Crayon', tool: pencilService });
+        this.tools.set('ellipse', { name: 'Ellipse', tool: ellipseService });
     }
 }
