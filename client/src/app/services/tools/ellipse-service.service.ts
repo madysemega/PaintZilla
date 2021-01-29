48056bbf678adde6c6b0da792b9a8aae2adb4a5b
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-28 19:36:49 -0500

    Added default startPoint and max angle constant to ellipse tool service

--- client/src/app/services/tools/ellipse-service.service.ts
+++ client/src/app/services/tools/ellipse-service.service.ts
@@ -1,59 +1,59 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
-import { DrawingService } from '../drawing/drawing.service';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 import { MouseButton } from './pencil-service';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root',
 })
 export class EllipseService extends Tool {
+    private readonly CIRCLE_MAX_ANGLE: number = 360;
 
-  private startPoint: Vec2;
+    private startPoint: Vec2 = { x: 0, y: 0 };
 
-  constructor(drawingService: DrawingService) {
-      super(drawingService);
-  }
+    constructor(drawingService: DrawingService) {
+        super(drawingService);
+    }
 
-  onMouseDown(event: MouseEvent): void {
-      this.mouseDown = event.button === MouseButton.Left;
-      if (this.mouseDown) {
-          this.mouseDownCoord = this.getPositionFromMouse(event);
-          this.startPoint = this.mouseDownCoord;
-      }
-  }
+    onMouseDown(event: MouseEvent): void {
+        this.mouseDown = event.button === MouseButton.Left;
+        if (this.mouseDown) {
+            this.mouseDownCoord = this.getPositionFromMouse(event);
+            this.startPoint = this.mouseDownCoord;
+        }
+    }
 
-  onMouseUp(event: MouseEvent): void {
-      if (this.mouseDown) {
-          const mousePosition = this.getPositionFromMouse(event);
-          this.drawEllipse(this.drawingService.baseCtx, this.startPoint, mousePosition);
-      }
-      this.mouseDown = false;
-  }
+    onMouseUp(event: MouseEvent): void {
+        if (this.mouseDown) {
+            const mousePosition = this.getPositionFromMouse(event);
+            this.drawEllipse(this.drawingService.baseCtx, this.startPoint, mousePosition);
+        }
+        this.mouseDown = false;
+    }
 
-  onMouseMove(event: MouseEvent): void {
-      if (this.mouseDown) {
-          const mousePosition = this.getPositionFromMouse(event);
-          
-          this.drawingService.clearCanvas(this.drawingService.previewCtx);
-          this.drawEllipse(this.drawingService.previewCtx, this.startPoint, mousePosition);
-      }
-  }
+    onMouseMove(event: MouseEvent): void {
+        if (this.mouseDown) {
+            const mousePosition = this.getPositionFromMouse(event);
 
-  private drawEllipse(ctx: CanvasRenderingContext2D, startPoint: Vec2, endPoint: Vec2): void {
-      let center: Vec2 = {
-        x: (startPoint.x + endPoint.x) / 2,
-        y: (startPoint.y + endPoint.y) / 2
-      };
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            this.drawEllipse(this.drawingService.previewCtx, this.startPoint, mousePosition);
+        }
+    }
 
-      let radii: Vec2 = {
-        x: Math.abs(endPoint.x - startPoint.x) / 2,
-        y: Math.abs(endPoint.y - startPoint.y) / 2
-      };
+    private drawEllipse(ctx: CanvasRenderingContext2D, startPoint: Vec2, endPoint: Vec2): void {
+        const center: Vec2 = {
+            x: (startPoint.x + endPoint.x) / 2,
+            y: (startPoint.y + endPoint.y) / 2,
+        };
 
-      ctx.beginPath();
-      ctx.ellipse(center.x, center.y, radii.x, radii.y, 0, 0, 360);
-      ctx.stroke();
-  }
-  
+        const radii: Vec2 = {
+            x: Math.abs(endPoint.x - startPoint.x) / 2,
+            y: Math.abs(endPoint.y - startPoint.y) / 2,
+        };
+
+        ctx.beginPath();
+        ctx.ellipse(center.x, center.y, radii.x, radii.y, 0, 0, this.CIRCLE_MAX_ANGLE);
+        ctx.stroke();
+    }
 }
