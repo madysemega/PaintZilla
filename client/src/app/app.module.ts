e170be83bed8533829699d787b2009c76cb79255
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-30 10:23:35 -0500

    Imported menu, select and button toggler from material into app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
@@ -13,9 +16,10 @@ import { DrawingComponent } from './components/drawing/drawing.component';
 import { EditorComponent } from './components/editor/editor.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { EllipseToolConfigurationComponent } from './components/tool-configurations/ellipse-tool-configuration/ellipse-tool-configuration.component';
 
 @NgModule({
-    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent],
+    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent, EllipseToolConfigurationComponent],
     imports: [
         BrowserModule,
         HttpClientModule,
@@ -26,6 +30,9 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
         MatIconModule,
         MatTooltipModule,
         MatDividerModule,
+        MatMenuModule,
+        MatSelectModule,
+        MatButtonToggleModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
