b9e18d0fb314c1b75ed7e7683cb0aeb1cd325798
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-01-30 10:28:34 -0500

    Ellipse tool can now draw contoured, filled and contoured-and-filled ellipses

--- client/src/app/services/tools/ellipse-service.service.ts
+++ client/src/app/services/tools/ellipse-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ShapeType } from '@app/classes/shape-type';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
@@ -11,9 +12,11 @@ export class EllipseService extends Tool {
     private readonly CIRCLE_MAX_ANGLE: number = 360;
 
     private startPoint: Vec2 = { x: 0, y: 0 };
+    shapeType: ShapeType;
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
+        this.shapeType = ShapeType.Contoured;
     }
 
     onMouseDown(event: MouseEvent): void {
@@ -38,7 +41,30 @@ export class EllipseService extends Tool {
 
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
             this.drawEllipse(this.drawingService.previewCtx, this.startPoint, mousePosition);
+            this.drawPerimeter(this.drawingService.previewCtx, this.startPoint, mousePosition);
         }
+    }
+
+    private drawPerimeter(ctx: CanvasRenderingContext2D, startPoint: Vec2, endPoint: Vec2): void {
+        const DASH_NUMBER = 8;
+
+        const topLeft: Vec2 = {
+            x: Math.min(startPoint.x, endPoint.x),
+            y: Math.min(startPoint.y, endPoint.y),
+        };
+
+        const dimensions: Vec2 = {
+            x: Math.abs(endPoint.x - startPoint.x),
+            y: Math.abs(endPoint.y - startPoint.y),
+        };
+
+        ctx.save();
+        ctx.beginPath();
+        ctx.setLineDash([DASH_NUMBER]);
+        ctx.strokeStyle = '#888';
+        ctx.rect(topLeft.x, topLeft.y, dimensions.x, dimensions.y);
+        ctx.stroke();
+        ctx.restore();
     }
 
     private drawEllipse(ctx: CanvasRenderingContext2D, startPoint: Vec2, endPoint: Vec2): void {
@@ -52,8 +78,17 @@ export class EllipseService extends Tool {
             y: Math.abs(endPoint.y - startPoint.y) / 2,
         };
 
+        ctx.save();
         ctx.beginPath();
+        ctx.fillStyle = '#AAA';
+        ctx.strokeStyle = '#000';
         ctx.ellipse(center.x, center.y, radii.x, radii.y, 0, 0, this.CIRCLE_MAX_ANGLE);
-        ctx.stroke();
+        if (this.shapeType === ShapeType.Contoured || this.shapeType === ShapeType.ContouredAndFilled) {
+            ctx.stroke();
+        }
+        if (this.shapeType === ShapeType.Filled || this.shapeType === ShapeType.ContouredAndFilled) {
+            ctx.fill();
+        }
+        ctx.restore();
     }
 }
