b646b102bc1d2605abfe5cacc3587c97cd20ff00
NeoIsThere <samuel.sam.956@gmail.com>
2021-02-04 15:29:16 -0500

    fixed lint

--- client/src/app/tools/services/tools/ellipse-service.service.spec.ts
+++ client/src/app/tools/services/tools/ellipse-service.service.spec.ts
@@ -218,7 +218,8 @@ describe('EllipseService', () => {
         expect(service.lineWidth).toBe(lineWidth);
     });
 
-    it('onKeyDown should set isShiftDown to true if event was triggered from shift key', () => {/////
+    it('onKeyDown should set isShiftDown to true if event was triggered from shift key', () => {
+        /////
         const keyboardEvent: KeyboardEvent = {
             key: 'Shift',
         } as KeyboardEvent;
@@ -246,7 +247,8 @@ describe('EllipseService', () => {
         expect(service.isShiftDown).toBe(true);
     });
 
-    it('onKeyUp should set isShiftDown to false if event was triggered from shift key', () => {////
+    it('onKeyUp should set isShiftDown to false if event was triggered from shift key', () => {
+        ////
         const keyboardEvent: KeyboardEvent = {
             key: 'Shift',
         } as KeyboardEvent;
