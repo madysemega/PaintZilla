ed05c747f6ac3edabc4cf3074793eac09bec25f1
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-08 14:31:50 -0500

    Modified line service so that it uses a line shape renderer

--- client/src/app/tools/services/tools/line.service.ts
+++ client/src/app/tools/services/tools/line.service.ts
@@ -1,107 +1,62 @@
 import { Injectable } from '@angular/core';
 import { Vec2 } from '@app/app/classes/vec2';
 import { DrawingService } from '@app/drawing/services/drawing/drawing.service';
+import { LineShape } from '@app/shapes/line-shape';
+import { LineShapeRenderer } from '@app/shapes/renderers/line-shape-renderer';
+import { MouseButton } from '@app/tools/classes/mouse-button';
 import { Tool } from '@app/tools/classes/tool';
 
 @Injectable({
     providedIn: 'root',
 })
 export class LineService extends Tool {
-    private vertices: Vec2[];
+    private lineShape: LineShape;
+    private lineShapeRenderer: LineShapeRenderer;
     private lastMousePosition: Vec2;
 
-    private addVertex(newVertex: Vec2): void {
-        this.vertices.push(newVertex);
-        this.renderPreview();
-    }
-
-    private removeLastVertex(): void {
-        this.vertices.pop();
-        this.renderPreview();
-    }
-
-    private clearVertices(): void {
-        this.vertices = [];
-    }
-
-    private renderFinal(): void {
-        this.drawSegments(this.drawingService.baseCtx, this.vertices);
-    }
-
-    private renderPreview(): void {
-        this.drawingService.clearCanvas(this.drawingService.previewCtx);
-        this.drawSegments(this.drawingService.previewCtx, this.vertices);
-        this.drawSegments(this.drawingService.previewCtx, [this.vertices[this.vertices.length - 1], this.lastMousePosition]);
-    }
-
-    private isShapeBeingDrawn(): boolean {
-        return this.vertices.length !== 0;
-    }
-
-    private isShapeCloseable(): boolean {
-        const VALID_SHAPE_MIN_NB_VERTICES = 3;
-        const CLOSING_SEGMENT_LENGTH_VALIDITY_THRESHOLD = 20; // (in pixels)
-
-        const shapeHasEnoughVertices = this.vertices.length >= VALID_SHAPE_MIN_NB_VERTICES;
-        if (!shapeHasEnoughVertices) {
-            return false;
-        }
-
-        const firstVertex: Vec2 = this.vertices[0];
-        const mousePosition: Vec2 = this.lastMousePosition;
-
-        const closingSegmentLength: number = Math.sqrt((mousePosition.x - firstVertex.x) ** 2 + (mousePosition.y - firstVertex.y) ** 2);
-
-        return closingSegmentLength <= CLOSING_SEGMENT_LENGTH_VALIDITY_THRESHOLD;
-    }
-
-    private closeShape(): void {
-        const firstVertex: Vec2 = this.vertices[0];
-        this.addVertex(firstVertex);
-    }
-
     onMouseClick(event: MouseEvent): void {
-        const mousePosition: Vec2 = this.getPositionFromMouse(event);
-        this.addVertex(mousePosition);
+        if (event.button === MouseButton.Left) {
+            const mousePosition: Vec2 = this.getPositionFromMouse(event);
+            this.lineShape.vertices.push(mousePosition);
+
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            this.lineShapeRenderer.render(this.drawingService.previewCtx);
+        }
     }
 
     onMouseDoubleClick(event: MouseEvent): void {
-        this.removeLastVertex();
-        this.removeLastVertex();
+        if (event.button === MouseButton.Left) {
+            this.lineShape.vertices.length -= 2;
 
-        if (this.isShapeCloseable()) {
-            this.closeShape();
-        } else {
-            this.addVertex(this.lastMousePosition);
+            if (this.lineShape.isCloseableWith(this.lastMousePosition)) {
+                this.lineShape.close();
+            } else {
+                this.lineShape.vertices.push(this.lastMousePosition);
+            }
+
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+            this.lineShapeRenderer.render(this.drawingService.baseCtx);
+            this.lineShape.clear();
         }
-
-        this.drawingService.clearCanvas(this.drawingService.previewCtx);
-        this.renderFinal();
-        this.clearVertices();
     }
 
     onMouseMove(event: MouseEvent): void {
         const mousePosition: Vec2 = this.getPositionFromMouse(event);
         this.lastMousePosition = mousePosition;
-        if (this.isShapeBeingDrawn()) {
-            this.renderPreview();
+
+        const isShapeBeingDrawn = this.lineShape.vertices.length !== 0;
+        if (isShapeBeingDrawn) {
+            this.drawingService.clearCanvas(this.drawingService.previewCtx);
+
+            this.lineShape.vertices.push(mousePosition);
+            this.lineShapeRenderer.render(this.drawingService.previewCtx);
+            this.lineShape.vertices.pop();
         }
-    }
-
-    private drawSegments(ctx: CanvasRenderingContext2D, vertices: Vec2[]): void {
-        ctx.save();
-        ctx.beginPath();
-
-        for (const vertex of vertices) {
-            ctx.lineTo(vertex.x, vertex.y);
-        }
-
-        ctx.stroke();
-        ctx.restore();
     }
 
     constructor(drawingService: DrawingService) {
         super(drawingService);
-        this.clearVertices();
+        this.lineShape = new LineShape([], []);
+        this.lineShapeRenderer = new LineShapeRenderer(this.lineShape);
     }
 }
