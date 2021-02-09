730789fc20aa2dcf65a6a16b324d8441e760eb6f
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-08 22:24:17 -0500

    Split test for line shape renderer render() method in three

--- client/src/app/shapes/renderers/line-shape-renderer.spec.ts
+++ client/src/app/shapes/renderers/line-shape-renderer.spec.ts
@@ -1,9 +1,10 @@
-import { CanvasTestHelper } from "@app/app/classes/canvas-test-helper";
-import { Vec2 } from "@app/app/classes/vec2";
-import { LineShape } from "../line-shape";
-import { ShapeProperty } from "../properties/shape-property";
-import { LineShapeRenderer } from "./line-shape-renderer";
+import { CanvasTestHelper } from '@app/app/classes/canvas-test-helper';
+import { Vec2 } from '@app/app/classes/vec2';
+import { LineShape } from '@app/shapes/line-shape';
+import { ShapeProperty } from '@app/shapes/properties/shape-property';
+import { LineShapeRenderer } from './line-shape-renderer';
 
+// tslint:disable:no-any
 describe('LineShapeRenderer', () => {
     let lineShapeRenderer: LineShapeRenderer;
     let lineShape: LineShape;
@@ -29,17 +30,35 @@ describe('LineShapeRenderer', () => {
         ctxStrokeSpy = spyOn<any>(ctxStub, 'stroke').and.callThrough();
     });
 
-    it('should draw with lineTo for every vertex', () => {
-        vertices.push({x: 0, y: 0});
-        vertices.push({x: 0, y: 20});
-        vertices.push({x: 20, y: 20});
+    it('should call beginPath once when render() is called', () => {
+        vertices.push({ x: 0, y: 0 });
+        vertices.push({ x: 0, y: 20 });
+        vertices.push({ x: 20, y: 20 });
 
         lineShapeRenderer.render(ctxStub);
-        
+
         expect(ctxBeginPathSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call stroke once when render() is called', () => {
+        vertices.push({ x: 0, y: 0 });
+        vertices.push({ x: 0, y: 20 });
+        vertices.push({ x: 20, y: 20 });
+
+        lineShapeRenderer.render(ctxStub);
+
+        expect(ctxStrokeSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call lineTo for every vertex when render() is called', () => {
+        vertices.push({ x: 0, y: 0 });
+        vertices.push({ x: 0, y: 20 });
+        vertices.push({ x: 20, y: 20 });
+
+        lineShapeRenderer.render(ctxStub);
+
         expect(ctxLineToSpy).toHaveBeenCalledWith(vertices[0].x, vertices[0].y);
         expect(ctxLineToSpy).toHaveBeenCalledWith(vertices[1].x, vertices[1].y);
         expect(ctxLineToSpy).toHaveBeenCalledWith(vertices[2].x, vertices[2].y);
-        expect(ctxStrokeSpy).toHaveBeenCalledTimes(1);
     });
 });
