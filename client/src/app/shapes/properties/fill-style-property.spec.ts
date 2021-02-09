e4b510630f5c241fc37654ae437f739b9822dfc4
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-08 22:18:31 -0500

    Added test file for fill style property class

---
+++ client/src/app/shapes/properties/fill-style-property.spec.ts
@@ -0,0 +1,23 @@
+import { CanvasTestHelper } from '@app/app/classes/canvas-test-helper';
+import { FillStyleProperty } from './fill-style-property';
+
+describe('FillStyleProperty', () => {
+    let fillStyleProperty: FillStyleProperty;
+
+    const fillStyle = '#424242';
+
+    let canvasTestHelper: CanvasTestHelper;
+    let ctxStub: CanvasRenderingContext2D;
+
+    beforeEach(() => {
+        fillStyleProperty = new FillStyleProperty(fillStyle);
+
+        canvasTestHelper = new CanvasTestHelper();
+        ctxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+    });
+
+    it("when apply() is called, it should set the canvas rendering context's fillStyle property to the colour specified upon instantiation", () => {
+        fillStyleProperty.apply(ctxStub);
+        expect(ctxStub.fillStyle).toBe(fillStyle);
+    });
+});
