70957951de7ad81f902da618c96da504dcd8864a
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-10 15:02:39 -0500

    Added component for line tool configuration options

---
+++ client/src/app/tools/components/tool-configurations/line-tool-configuration/line-tool-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line-tool-configuration',
+  templateUrl: './line-tool-configuration.component.html',
+  styleUrls: ['./line-tool-configuration.component.scss']
+})
+export class LineToolConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
