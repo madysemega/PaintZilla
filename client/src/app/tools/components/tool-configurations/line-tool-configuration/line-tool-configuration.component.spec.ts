b52cd8d13a3255724b543feed63c65b575b4b2f8
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-11 13:11:24 -0500

    Added tests for line tool configuration component line type and joint diameter event handling

--- client/src/app/tools/components/tool-configurations/line-tool-configuration/line-tool-configuration.component.spec.ts
+++ client/src/app/tools/components/tool-configurations/line-tool-configuration/line-tool-configuration.component.spec.ts
@@ -1,14 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatButtonToggleGroup } from '@angular/material/button-toggle';
+import { LineType } from '@app/shapes/types/line-type';
+import { LineService } from '@app/tools/services/tools/line.service';
 import { LineToolConfigurationComponent } from './line-tool-configuration.component';
 
 describe('LineToolConfigurationComponent', () => {
+    // tslint:disable-next-line: no-magic-numbers
+    const SAMPLE_DIAMETERS = [5, 1, 52, 42];
+    const LINE_TYPES = [LineType.WITH_JOINTS, LineType.WITHOUT_JOINTS];
+
     let component: LineToolConfigurationComponent;
     let fixture: ComponentFixture<LineToolConfigurationComponent>;
+    let lineServiceStub: jasmine.SpyObj<LineService>;
 
     beforeEach(async(() => {
+        lineServiceStub = jasmine.createSpyObj('LineService', ['setLineType', 'setJointsDiameter']);
+
         TestBed.configureTestingModule({
             declarations: [LineToolConfigurationComponent],
+            providers: [{ provide: LineService, useValue: lineServiceStub }],
         }).compileComponents();
     }));
 
@@ -20,5 +30,39 @@ describe('LineToolConfigurationComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+
+    it('onLineTypeChange() should change the lineType attribute accordingly', () => {
+        LINE_TYPES.forEach((initialType) => {
+            LINE_TYPES.forEach((giveType) => {
+                component.lineType = initialType;
+                component.onLineTypeChange({ value: giveType } as MatButtonToggleGroup);
+                expect(component.lineType).toEqual(giveType);
+            });
+        });
+    });
+
+    it('onLineTypeChange() should propagate event to line service with given parameter', () => {
+        LINE_TYPES.forEach((lineType) => {
+            component.onLineTypeChange({ value: lineType } as MatButtonToggleGroup);
+            expect(lineServiceStub.setLineType).toHaveBeenCalledWith(lineType);
+        });
+    });
+
+    it('onJointsDiameterChange() should change the jointsDiameter attribute accordingly', () => {
+        SAMPLE_DIAMETERS.forEach((initialDiameter) => {
+            SAMPLE_DIAMETERS.forEach((givenDiameter) => {
+                component.jointsDiameter = initialDiameter;
+                component.onJointsDiameterChange(givenDiameter);
+                expect(component.jointsDiameter).toEqual(givenDiameter);
+            });
+        });
+    });
+
+    it('onJointsDiameterChange() should propagate the event to line service with given parameter', () => {
+        SAMPLE_DIAMETERS.forEach((diameter) => {
+            component.onJointsDiameterChange(diameter);
+            expect(lineServiceStub.setJointsDiameter).toHaveBeenCalledWith(diameter);
+        });
     });
 });
