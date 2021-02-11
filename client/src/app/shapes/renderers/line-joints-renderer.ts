911c851f6308fd5dddd26dd235e02d09550052eb
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-11 13:08:14 -0500

    Added line joints renderer to render the points on the vertices

---
+++ client/src/app/shapes/renderers/line-joints-renderer.ts
@@ -0,0 +1,21 @@
+import { LineShape } from '@app/shapes/line-shape';
+import { ShapeProperty } from '@app/shapes/properties/shape-property';
+import { ShapeRenderer } from './shape-renderer';
+
+export class LineJointsRenderer extends ShapeRenderer<LineShape> {
+    draw(ctx: CanvasRenderingContext2D): void {
+        const FULL_CIRCLE_DEGREES = 360;
+
+        const radius = this.shape.jointsDiameter / 2;
+
+        this.shape.vertices.forEach((vertex) => {
+            ctx.beginPath();
+            ctx.ellipse(vertex.x, vertex.y, radius, radius, 0, 0, FULL_CIRCLE_DEGREES);
+            ctx.fill();
+        });
+    }
+
+    constructor(shape: LineShape, properties: ShapeProperty[]) {
+        super(shape, properties);
+    }
+}
