3e21dd6eb41fcc97f33f89bafbb15174ebffd764
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-13 14:26:20 -0500

    Resizable tool configuration component now binds directly to the service's line width attribute

--- client/src/app/tools/components/tool-configurations/resizable-tool-configuration/resizable-tool-configuration.component.ts
+++ client/src/app/tools/components/tool-configurations/resizable-tool-configuration/resizable-tool-configuration.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ResizableTool } from '@app/app/classes/resizable-tool';
-import { StrokeWidthProperty } from '@app/shapes/properties/stroke-width-property';
 import { ToolSelectorService } from '@app/tools/services/tool-selector/tool-selector.service';
 
 @Component({
@@ -8,22 +7,18 @@ import { ToolSelectorService } from '@app/tools/services/tool-selector/tool-sele
     templateUrl: './resizable-tool-configuration.component.html',
     styleUrls: ['./resizable-tool-configuration.component.scss'],
 })
-export class ResizableToolConfigurationComponent implements OnInit {
+export class ResizableToolConfigurationComponent {
     @Input() toolService: ResizableTool;
-    lineWidth: number;
     info: string = 'Épaisseur du trait';
     position: string = 'right';
 
-    constructor(public toolSelectorService: ToolSelectorService) {
-        this.lineWidth = StrokeWidthProperty.DEFAULT_STROKE_WIDTH;
+    get lineWidth(): number {
+        return this.toolService.lineWidth;
     }
 
     changeWidth(width: number): void {
-        this.toolService.adjustLineWidth(width);
-        this.lineWidth = width;
+        this.toolService.lineWidth = width;
     }
 
-    ngOnInit(): void {
-        this.lineWidth = this.toolService.lineWidth;
-    }
+    constructor(public toolSelectorService: ToolSelectorService) {}
 }
