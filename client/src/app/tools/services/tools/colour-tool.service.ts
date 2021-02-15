44006b27cfb3cf45e4e08b6b61c21b3ed7e01b60
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-15 18:50:05 -0500

    Renamed colour1 and colour2 to primaryColour and secondaryColour

--- client/src/app/tools/services/tools/colour-tool.service.ts
+++ client/src/app/tools/services/tools/colour-tool.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class ColourToolService {
-    colour1: string;
-    colour2: string;
+    primaryColour: string;
+    secondaryColour: string;
     colourList: string[] = [];
 }
