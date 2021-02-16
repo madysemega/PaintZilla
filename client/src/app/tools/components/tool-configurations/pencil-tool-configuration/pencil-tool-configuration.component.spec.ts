f55c7e9f60696d7f1217b97f7b4437b4b74ac421
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-02-15 19:27:36 -0500

    Added colour service stub to tool component test files

--- client/src/app/tools/components/tool-configurations/pencil-tool-configuration/pencil-tool-configuration.component.spec.ts
+++ client/src/app/tools/components/tool-configurations/pencil-tool-configuration/pencil-tool-configuration.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DrawingService } from '@app/drawing/services/drawing-service/drawing.service';
 import { PencilToolConfigurationComponent } from '@app/tools/components/tool-configurations/pencil-tool-configuration/pencil-tool-configuration.component';
+import { ColourToolService } from '@app/tools/services/tools/colour-tool.service';
 import { PencilService } from '@app/tools/services/tools/pencil-service';
 
 describe('PencilToolConfigurationComponent', () => {
@@ -8,11 +9,13 @@ describe('PencilToolConfigurationComponent', () => {
     let fixture: ComponentFixture<PencilToolConfigurationComponent>;
 
     let drawingServiceStub: DrawingService;
+    let colourServiceStub: ColourToolService;
     let pencilServiceStub: PencilService;
 
     beforeEach(async(() => {
         drawingServiceStub = new DrawingService();
-        pencilServiceStub = new PencilService(drawingServiceStub);
+        colourServiceStub = new ColourToolService();
+        pencilServiceStub = new PencilService(drawingServiceStub, colourServiceStub);
 
         TestBed.configureTestingModule({
             declarations: [PencilToolConfigurationComponent],
