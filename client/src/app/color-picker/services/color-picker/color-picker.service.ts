7560b1b517480ce61d4230fc47e18a9133ab9f19
Mady <madysemega.ms@gmail.com>
2021-02-20 08:07:27 -0500

    Added hsv observables to Color picker service

--- client/src/app/color-picker/services/color-picker/color-picker.service.ts
+++ client/src/app/color-picker/services/color-picker/color-picker.service.ts
@@ -4,9 +4,22 @@ import { BehaviorSubject } from 'rxjs';
 
 @Injectable()
 export class ColorPickerService {
-    private currentColor: ColorConverter;
-    private hueChangedSubject: BehaviorSubject<number>;
-    private saturationChangedSource: BehaviorSubject<number>;
-    private valueChangedSubject: BehaviorSubject<number>;
-    private alphaChangedSource: BehaviorSubject<number>;
+    private currentColor: ColorConverter = new ColorConverter();
+    private hueChangedSubject: BehaviorSubject<number> = new BehaviorSubject<number>(this.currentColor.getHSV()[0]);
+    private saturationChangedSubject: BehaviorSubject<number> = new BehaviorSubject<number>(this.currentColor.getHSV()[1]);
+    private valueChangedSubject: BehaviorSubject<number> = new BehaviorSubject<number>(this.currentColor.getHSV()[2]);
+    private alphaChangedSubject: BehaviorSubject<number> = new BehaviorSubject<number>(this.currentColor.alpha);
+
+    hueChangedObservable = this.hueChangedSubject.asObservable();
+    saturationChangedObservable = this.saturationChangedSubject.asObservable();
+    valueChangedObservable = this.valueChangedSubject.asObservable();
+    alphachangedObservable = this.alphaChangedSubject.asObservable();
+
+    getColor(): ColorConverter {
+        return this.currentColor;
+    }
+
+    setColor(color: ColorConverter): void {
+        
+    }
 }
