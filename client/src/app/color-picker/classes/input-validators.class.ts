8740cb085a4b594189627596410cc27d787086a8
Mady <madysemega.ms@gmail.com>
2021-02-20 23:57:43 -0500

    Removed color converter and regexes for new Architecture

--- client/src/app/color-picker/classes/input-validators.class.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    RGB_VALIDATOR: /^rgb\((\d{1,3}(?:\.\d+)?),\s*(\d{1,3}(?:\.\d+)?),\s*(\d{1,3}(?:\.\d+)?)\)$/,
-    RGBA_VALIDATOR: /^rgba\((\d{1,3}(?:\.\d+)?),\s*(\d{1,3}(?:\.\d+)?),\s*(\d{1,3}(?:\.\d+)?),\s*(\d+(?:\.\d+)?)\)$/,
-    HEX_TWO_VALIDATOR: /^[0-9a-fA-F]{2}$/,
-    HEX_SIX_VALIDATOR: /^[0-9a-fA-F]{6}$/,
-    INT_VALIDATOR: /^[0-9]*$/,
-    DEC_VALIDATOR: /^\d+\.?\d*$/,
-};
--- client/src/app/color-picker/constants/color-slider.constants.ts
+++
@@ -1,2 +0,0 @@
-export const CANVAS_WIDTH = 20;
-export const CANVAS_HEIGHT = 200;
