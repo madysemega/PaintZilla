0f4199950e5c39412e222d9df825a23a2d5137b7
NeoIsThere <samuel.sam.956@gmail.com>
2021-02-22 01:58:28 -0500

    horizontal and vertical resizing seem to be working.

--- client/src/app/tools/services/tools/ellipse-selection-handler-service.ts
+++ client/src/app/tools/services/tools/ellipse-selection-handler-service.ts
@@ -18,9 +18,13 @@ export class EllipseSelectionHandlerService {
   private readonly CIRCLE_MAX_ANGLE: number = 360;
 
   public selectionCanvas: HTMLCanvasElement;
-  public modificationCanvas: HTMLCanvasElement;
+  public horizontalModificationCanvas: HTMLCanvasElement;
+  public verticalModificationCanvas: HTMLCanvasElement;
+  public originalCanvasCopy: HTMLCanvasElement;
   public selectionCtx: CanvasRenderingContext2D;
-  public modificationCtx: CanvasRenderingContext2D;
+  public horizontalModificationCtx: CanvasRenderingContext2D;
+  public verticalModificationCtx: CanvasRenderingContext2D;
+  public originalCanvasCopyCtx: CanvasRenderingContext2D;
   public topLeft: Vec2 = { x: 0, y: 0 };
   public offset: Vec2 = { x: 0, y: 0 };
   public originalWidth: number;
@@ -34,8 +38,12 @@ export class EllipseSelectionHandlerService {
   constructor(private drawingService: DrawingService, private selectionService: SelectionService) {
     this.selectionCanvas = document.createElement('canvas');
     this.selectionCtx = this.selectionCanvas.getContext('2d') as CanvasRenderingContext2D
-    this.modificationCanvas = document.createElement('canvas');
-    this.modificationCtx = this.modificationCanvas.getContext('2d') as CanvasRenderingContext2D
+    this.originalCanvasCopy =  document.createElement('canvas');
+    this.originalCanvasCopyCtx = this.originalCanvasCopy.getContext('2d') as CanvasRenderingContext2D;
+    this.horizontalModificationCanvas = document.createElement('canvas');
+    this.horizontalModificationCtx = this.horizontalModificationCanvas.getContext('2d') as CanvasRenderingContext2D
+    this.verticalModificationCanvas = document.createElement('canvas');
+    this.verticalModificationCtx = this.verticalModificationCanvas.getContext('2d') as CanvasRenderingContext2D
   }
 
   selectArea(selectionStartPoint: Vec2, center: Vec2, radii: Vec2): void {
@@ -50,10 +58,20 @@ export class EllipseSelectionHandlerService {
     this.selectionCtx.closePath();
     this.selectionCtx.restore();
 
-    this.modificationCtx.beginPath();
-    this.modificationCtx.imageSmoothingEnabled = false;
-    this.modificationCtx.drawImage(this.selectionCanvas, 0, 0);
-    this.modificationCtx.closePath();
+    this.horizontalModificationCtx.beginPath();
+    this.horizontalModificationCtx.imageSmoothingEnabled = false;
+    this.horizontalModificationCtx.drawImage(this.selectionCanvas, 0, 0);
+    this.horizontalModificationCtx.closePath();
+
+    this.verticalModificationCtx.beginPath();
+    this.verticalModificationCtx.imageSmoothingEnabled = false;
+    this.verticalModificationCtx.drawImage(this.selectionCanvas, 0, 0);
+    this.verticalModificationCtx.closePath();
+
+    this.originalCanvasCopyCtx.beginPath();
+    this.originalCanvasCopyCtx.imageSmoothingEnabled = false;
+    this.originalCanvasCopyCtx.drawImage(this.selectionCanvas, 0, 0);
+    this.originalCanvasCopyCtx.closePath();
 
     this.topLeft.x = this.selectionCanvas.width / 2 - radii.x;
     this.topLeft.y = this.selectionCanvas.height / 2 - radii.y;
@@ -82,14 +100,24 @@ export class EllipseSelectionHandlerService {
   resizeSelectionHorizontally(topLeft: Vec2, bottomRight: Vec2): void {
     let newWidth = Math.abs(topLeft.x - bottomRight.x);
     let increaseRatio = newWidth / this.originalWidth;
+
     this.drawingService.clearCanvas(this.selectionCtx);
     this.selectionCtx.beginPath();
     this.selectionCtx.translate(this.selectionCanvas.width / 2, this.selectionCanvas.height / 2);
     this.selectionCtx.transform(increaseRatio, 0, 0, 1, 0, 0);
     this.selectionCtx.translate(-this.selectionCanvas.width / 2, -this.selectionCanvas.height / 2);
-    this.selectionCtx.drawImage(this.modificationCanvas, 0, 0);
+    this.selectionCtx.drawImage(this.horizontalModificationCanvas, 0, 0);
     this.selectionCtx.closePath();
     this.selectionCtx.resetTransform();
+
+    this.drawingService.clearCanvas(this.verticalModificationCtx);
+    this.verticalModificationCtx.beginPath();
+    this.verticalModificationCtx.translate(this.selectionCanvas.width / 2, this.selectionCanvas.height / 2);
+    this.verticalModificationCtx.transform(increaseRatio, 0, 0, 1, 0, 0);
+    this.verticalModificationCtx.translate(-this.selectionCanvas.width / 2, -this.selectionCanvas.height / 2);
+    this.verticalModificationCtx.drawImage(this.originalCanvasCopy,0,0);
+    this.verticalModificationCtx.closePath();
+    this.verticalModificationCtx.resetTransform();
 
     this.hasBeenResized = true;
     this.updateHorizontalOffset(newWidth);
@@ -104,9 +132,18 @@ export class EllipseSelectionHandlerService {
     this.selectionCtx.translate(this.selectionCanvas.width / 2, this.selectionCanvas.height / 2);
     this.selectionCtx.transform(1, 0, 0, increaseRatio, 0, 0);
     this.selectionCtx.translate(-this.selectionCanvas.width / 2, -this.selectionCanvas.height / 2);
-    this.selectionCtx.drawImage(this.modificationCanvas, 0, 0);
+    this.selectionCtx.drawImage(this.verticalModificationCanvas, 0, 0);
     this.selectionCtx.closePath();
     this.selectionCtx.resetTransform();
+
+    this.drawingService.clearCanvas(this.horizontalModificationCtx);
+    this.horizontalModificationCtx.beginPath();
+    this.horizontalModificationCtx.translate(this.selectionCanvas.width / 2, this.selectionCanvas.height / 2);
+    this.horizontalModificationCtx.transform(1, 0, 0, increaseRatio, 0, 0);
+    this.horizontalModificationCtx.translate(-this.selectionCanvas.width / 2, -this.selectionCanvas.height / 2);
+    this.horizontalModificationCtx.drawImage(this.originalCanvasCopy,0,0);
+    this.horizontalModificationCtx.closePath();
+    this.horizontalModificationCtx.resetTransform();
 
     this.hasBeenResized = true;
     this.updateVerticalOffset(newHeight);
@@ -123,8 +160,12 @@ export class EllipseSelectionHandlerService {
   clearAndResetCanvas() {
     this.selectionCanvas.width = this.drawingService.canvas.width;
     this.selectionCanvas.height = this.drawingService.canvas.height;
-    this.modificationCanvas.width = this.drawingService.canvas.width;
-    this.modificationCanvas.height = this.drawingService.canvas.height;
+    this.originalCanvasCopy.width = this.drawingService.canvas.width;
+    this.originalCanvasCopy.height = this.drawingService.canvas.height;
+    this.horizontalModificationCanvas.width = this.drawingService.canvas.width;
+    this.horizontalModificationCanvas.height = this.drawingService.canvas.height;
+    this.verticalModificationCanvas.width = this.drawingService.canvas.width;
+    this.verticalModificationCanvas.height = this.drawingService.canvas.height;
   }
 
   hasSelectionBeenMoved(newStartPos: Vec2): boolean {
