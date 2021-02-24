a0cd2eaff662c8a88317d8a45a0820b87a67be80
Mady <madysemega.ms@gmail.com>
2021-02-23 22:06:48 -0500

    Added default colors

--- client/src/app/colour-picker/services/colour/colour.service.ts
+++ client/src/app/colour-picker/services/colour/colour.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-
+import * as Constants from '@app/colour-picker/constants/colour.service.constants';
 @Injectable({
     providedIn: 'root',
 })
-
 export class ColourService {}
