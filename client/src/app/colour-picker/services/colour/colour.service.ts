6cb7428fa002e61f9dc680a22acbea288e55e679
Mady <madysemega.ms@gmail.com>
2021-02-23 22:21:09 -0500

    added subjects and observables for primary and secondary colours

--- client/src/app/colour-picker/services/colour/colour.service.ts
+++ client/src/app/colour-picker/services/colour/colour.service.ts
@@ -1,6 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Colour } from '@app/colour-picker/classes/colours.class';
 import * as Constants from '@app/colour-picker/constants/colour.service.constants';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({
     providedIn: 'root',
 })
-export class ColourService {}
+export class ColourService {
+    private previousColours: Colour[];
+    private primaryColourSubject: BehaviorSubject<Colour>;
+    private secondaryColourSubject: BehaviorSubject<Colour>;
+    primaryColourObservable: Observable<Colour>;
+    secondaryColourObservable: Observable<Colour>;
+    constructor() {
+        this.primaryColourSubject = new BehaviorSubject<Colour>(Constants.DEFAULT_PRIMARY);
+        this.secondaryColourSubject = new BehaviorSubject<Colour>(Constants.DEFAULT_SECONDARY);
+        this.previousColours = Constants.INITIAL_COLORS;
+        this.primaryColourObservable = this.primaryColourSubject.asObservable();
+        this.secondaryColourObservable = this.secondaryColourSubject.asObservable();
+    }
+
+    getPrimaryColour(): string {
+        return this.primaryColourSubject.value.toStringHex();
+    }
+
+    getSecondaryColour(): string {
+        return this.secondaryColourSubject.value.toStringHex();
+    }
+
+    swapColours(): void {
+        const oldPrime = this.primaryColourSubject.value;
+        this.primaryColourSubject.next(this.secondaryColourSubject.value);
+        this.secondaryColourSubject.next(oldPrime);
+    }
+}
