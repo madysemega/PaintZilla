5146ba5b1cb47a57f2e92714f662a723ad034534
Felix Duguay <felixduguay99@gmail.com>
2021-03-03 12:11:31 -0500

    Commit avec merge

--- client/src/app/tools/components/tool-configurations/spray-tool-configuration/spray-tool-configuration.component.spec.ts
+++ client/src/app/tools/components/tool-configurations/spray-tool-configuration/spray-tool-configuration.component.spec.ts
@@ -1,22 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ColourPickerService } from '@app/colour-picker/services/colour-picker/colour-picker.service';
+import { ColourService } from '@app/colour-picker/services/colour/colour.service';
 import { DrawingService } from '@app/drawing/services/drawing-service/drawing.service';
+import { HistoryService } from '@app/history/service/history.service';
 import { MaterialModule } from '@app/material.module';
 import { ResizableToolConfigurationComponent } from '@app/tools/components/tool-configurations/resizable-tool-configuration/resizable-tool-configuration.component';
 import { SprayToolConfigurationComponent } from '@app/tools/components/tool-configurations/spray-tool-configuration/spray-tool-configuration.component';
-import { ColourToolService } from '@app/tools/services/tools/colour-tool.service';
 import { SprayService } from '@app/tools/services/tools/spray-service';
 
 describe('SprayToolConfigurationComponent', () => {
     let component: SprayToolConfigurationComponent;
     let fixture: ComponentFixture<SprayToolConfigurationComponent>;
 
+    let historyServiceStub: HistoryService;
     let drawingServiceStub: DrawingService;
-    let colourServiceStub: ColourToolService;
+    let colourServiceStub: ColourService;
     let sprayServiceStub: SprayService;
 
     beforeEach(async(() => {
-        drawingServiceStub = new DrawingService();
-        colourServiceStub = new ColourToolService();
+        historyServiceStub = new HistoryService();
+        drawingServiceStub = new DrawingService(historyServiceStub);
+        colourServiceStub = new ColourService({} as ColourPickerService);
         sprayServiceStub = new SprayService(drawingServiceStub, colourServiceStub);
 
         TestBed.configureTestingModule({
