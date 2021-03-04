a8a3d8e064f0fd667dd811b5132d393909deebf3
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-03-04 16:18:25 -0500

    Added renderer for vertices shape

---
+++ client/src/app/shapes/renderers/vertices-renderer.ts
@@ -0,0 +1,22 @@
+import { ShapeProperty } from '@app/shapes/properties/shape-property';
+import { VerticesShape } from '@app/shapes/vertices-shape';
+import { ShapeRenderer } from './shape-renderer';
+
+export class VerticesRenderer extends ShapeRenderer<VerticesShape> {
+    constructor(shape: VerticesShape, properties: ShapeProperty[]) {
+        super(shape, properties);
+    }
+
+    draw(ctx: CanvasRenderingContext2D): void {
+        ctx.beginPath();
+        console.log(ctx.strokeStyle);
+        this.shape.vertices.forEach((vertex) => {
+            ctx.lineTo(vertex.x, vertex.y);
+        });
+        ctx.stroke();
+    }
+
+    clone(): ShapeRenderer<VerticesShape> {
+        return new VerticesRenderer(this.getShapeCopy(), this.getPropertiesCopy());
+    }
+}
