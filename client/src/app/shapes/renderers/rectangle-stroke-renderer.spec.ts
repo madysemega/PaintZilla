372eeef01f6ab96dee7414417b966d1999be695c
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-03-05 00:42:39 -0500

    Added renderer for rectangle stroke

---
+++ client/src/app/shapes/renderers/rectangle-stroke-renderer.spec.ts
@@ -0,0 +1,47 @@
+import { CanvasTestHelper } from '@app/app/classes/canvas-test-helper';
+import { BoxShape } from '@app/shapes/box-shape';
+import { ShapeProperty } from '@app/shapes/properties/shape-property';
+import { RectangleStrokeRenderer } from './rectangle-stroke-renderer';
+
+// tslint:disable:no-any
+describe('RectangleStrokeRenderer', () => {
+    const INITIAL_TOP_LEFT = { x: 0, y: 0 };
+    const INITIAL_BOTTOM_RIGHT = { x: 32, y: 32 };
+
+    let renderer: RectangleStrokeRenderer;
+    let shape: BoxShape;
+    let properties: ShapeProperty[];
+
+    let canvasTestHelper: CanvasTestHelper;
+    let ctxStub: CanvasRenderingContext2D;
+    let ctxRectSpy: jasmine.Spy<any>;
+    let ctxBeginPathSpy: jasmine.Spy<any>;
+    let ctxStrokeSpy: jasmine.Spy<any>;
+
+    beforeEach(() => {
+        properties = new Array<ShapeProperty>();
+        shape = new BoxShape(INITIAL_TOP_LEFT, INITIAL_BOTTOM_RIGHT);
+        renderer = new RectangleStrokeRenderer(shape, properties);
+
+        canvasTestHelper = new CanvasTestHelper();
+        ctxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+        ctxRectSpy = spyOn<any>(ctxStub, 'rect').and.callThrough();
+        ctxBeginPathSpy = spyOn<any>(ctxStub, 'beginPath').and.callThrough();
+        ctxStrokeSpy = spyOn<any>(ctxStub, 'stroke').and.callThrough();
+    });
+
+    it('should call beginPath once when render() is called', () => {
+        renderer.render(ctxStub);
+        expect(ctxBeginPathSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call stroke once when render() is called', () => {
+        renderer.render(ctxStub);
+        expect(ctxStrokeSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call rect with shape position/dimensions when render() is called', () => {
+        renderer.render(ctxStub);
+        expect(ctxRectSpy).toHaveBeenCalledWith(shape.topLeft.x, shape.topLeft.y, shape.width, shape.height);
+    });
+});
