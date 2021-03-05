8242830a64b7ca93c3db92b63034482d2066b705
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-03-05 00:45:46 -0500

    Adapted rectangle tool to use renderers and to register user actions to history server

--- client/src/app/tools/services/tools/rectangle.service.spec.ts
+++ client/src/app/tools/services/tools/rectangle.service.spec.ts
@@ -2,12 +2,21 @@ import { TestBed } from '@angular/core/testing';
 import { CanvasTestHelper } from '@app/app/classes/canvas-test-helper';
 import { ShapeType } from '@app/app/classes/shape-type';
 import { Vec2 } from '@app/app/classes/vec2';
+import { Colour } from '@app/colour-picker/classes/colours.class';
+import { ColourPickerService } from '@app/colour-picker/services/colour-picker/colour-picker.service';
+import { ColourService } from '@app/colour-picker/services/colour/colour.service';
 import { DrawingService } from '@app/drawing/services/drawing-service/drawing.service';
+import { HistoryService } from '@app/history/service/history.service';
+import { UserActionRenderShape } from '@app/history/user-actions/user-action-render-shape';
 import { RectangleService } from './rectangle.service';
 
-// tslint:disable:no-any
+// tslint:disable: no-any
+// tslint:disable: no-string-literal
+// tslint:disable: max-file-line-count
 describe('RectangleService', () => {
     let service: RectangleService;
+    let history: HistoryService;
+    let colourService: ColourService;
     let mouseEvent: MouseEvent;
     let keyboardShiftEvent: KeyboardEvent;
     let keyboardSpaceEvent: KeyboardEvent;
@@ -16,20 +25,27 @@ describe('RectangleService', () => {
 
     let baseCtxStub: CanvasRenderingContext2D;
     let previewCtxStub: CanvasRenderingContext2D;
+
     let drawRectSpy: jasmine.Spy<any>;
-    let previewCtxStrokeSpy: jasmine.Spy<any>;
-    let previewCtxFillSpy: jasmine.Spy<any>;
-    let baseCtxStrokeSpy: jasmine.Spy<any>;
-    let baseCtxFillSpy: jasmine.Spy<any>;
+    let finalizeSpy: jasmine.Spy<any>;
+
+    let fillRenderSpy: jasmine.Spy<any>;
+    let strokeRenderSpy: jasmine.Spy<any>;
 
     let canvas: HTMLCanvasElement;
     let canvasPosition: Vec2;
 
     beforeEach(() => {
         drawServiceSpy = jasmine.createSpyObj('DrawingService', ['clearCanvas']);
+        history = new HistoryService();
+        colourService = new ColourService({} as ColourPickerService);
 
         TestBed.configureTestingModule({
-            providers: [{ provide: DrawingService, useValue: drawServiceSpy }],
+            providers: [
+                { provide: DrawingService, useValue: drawServiceSpy },
+                { provide: HistoryService, useValue: history },
+                { provide: ColourService, useValue: colourService },
+            ],
         });
         canvasTestHelper = TestBed.inject(CanvasTestHelper);
         baseCtxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -47,10 +63,10 @@ describe('RectangleService', () => {
         );
 
         drawRectSpy = spyOn<any>(service, 'drawRect').and.callThrough();
-        previewCtxStrokeSpy = spyOn<any>(previewCtxStub, 'strokeRect').and.callThrough();
-        previewCtxFillSpy = spyOn<any>(previewCtxStub, 'fillRect').and.callThrough();
-        baseCtxStrokeSpy = spyOn<any>(baseCtxStub, 'strokeRect').and.callThrough();
-        baseCtxFillSpy = spyOn<any>(baseCtxStub, 'fillRect').and.callThrough();
+        finalizeSpy = spyOn<any>(service, 'finalize').and.callThrough();
+
+        fillRenderSpy = spyOn<any>(service['fillRenderer'], 'render').and.callThrough();
+        strokeRenderSpy = spyOn<any>(service['strokeRenderer'], 'render').and.callThrough();
 
         // Configuration du spy du service
         // tslint:disable:no-string-literal
@@ -96,14 +112,6 @@ describe('RectangleService', () => {
         } as MouseEvent;
         service.onMouseDown(mouseEventRClick);
         expect(service.mouseDown).toEqual(false);
-    });
-
-    it(' onMouseUp should call drawRect if mouse was already down', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.onMouseUp(mouseEvent);
-        expect(drawRectSpy).toHaveBeenCalled();
     });
 
     it(' onMouseUp should not call drawRect if mouse was not already down', () => {
@@ -164,113 +172,6 @@ describe('RectangleService', () => {
         expect(drawRectSpy).not.toHaveBeenCalled();
     });
 
-    it(' onDeselectTool should call drawRect if mouseDown', () => {
-        service.mouseDown = true;
-        service.mouseDownCoord = { x: 0, y: 0 };
-
-        service.onToolDeselect();
-        expect(drawRectSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseMove should call stroke on preview canvas if shape type is Contoured', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.Contoured;
-        service.onMouseMove(mouseEvent);
-        expect(previewCtxStrokeSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseMove should call stroke on preview canvas if shape type is ContouredAndFilled', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.ContouredAndFilled;
-        service.onMouseMove(mouseEvent);
-        expect(previewCtxStrokeSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseMove should call fill on preview canvas if shape type is Filled', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.Filled;
-        service.onMouseMove(mouseEvent);
-        expect(previewCtxFillSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseMove should call fill on preview canvas if shape type is ContouredAndFilled', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.ContouredAndFilled;
-        service.onMouseMove(mouseEvent);
-        expect(previewCtxFillSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseMove should not call fill on preview canvas if shape type is Contoured', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.Contoured;
-        service.onMouseMove(mouseEvent);
-        expect(previewCtxFillSpy).not.toHaveBeenCalled();
-    });
-
-    it(' onMouseUp should call stroke on base canvas if shape type is Contoured', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.Contoured;
-        service.onMouseUp(mouseEvent);
-        expect(baseCtxStrokeSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseUp should call stroke on base canvas if shape type is ContouredAndFilled', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.ContouredAndFilled;
-        service.onMouseUp(mouseEvent);
-        expect(baseCtxStrokeSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseUp should call fill on base canvas if shape type is Filled', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.Filled;
-        service.onMouseUp(mouseEvent);
-        expect(baseCtxFillSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseUp should call fill on base canvas if shape type is ContouredAndFilled', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.ContouredAndFilled;
-        service.onMouseUp(mouseEvent);
-        expect(baseCtxFillSpy).toHaveBeenCalled();
-    });
-
-    it(' onMouseUp should not call fill on base canvas if shape type is Contoured', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.Contoured;
-        service.onMouseUp(mouseEvent);
-        expect(baseCtxFillSpy).not.toHaveBeenCalled();
-    });
-
-    it(' onMouseUp should not call stroke on base canvas if shape type is Filled', () => {
-        service.mouseDownCoord = { x: 0, y: 0 };
-        service.mouseDown = true;
-
-        service.shapeType = ShapeType.Filled;
-        service.onMouseUp(mouseEvent);
-        expect(baseCtxStrokeSpy).not.toHaveBeenCalled();
-    });
-
     it(' after having been adjusted width should be negative if it was so before the adjustment', () => {
         const x = 0;
         const y = 11;
@@ -319,6 +220,18 @@ describe('RectangleService', () => {
         expect(service.height).toBeGreaterThan(0);
     });
 
+    it('when line width changes, stroke width property should as well', () => {
+        const INITIAL_LINE_WIDTH = 1;
+        const NEW_LINE_WIDTH = 3;
+
+        service['strokeWidthProperty'].strokeWidth = INITIAL_LINE_WIDTH;
+
+        service.lineWidth = NEW_LINE_WIDTH;
+        service.onLineWidthChanged();
+
+        expect(service['strokeWidthProperty'].strokeWidth).toEqual(NEW_LINE_WIDTH);
+    });
+
     it(' should change the pixel of the canvas ', () => {
         mouseEvent = { clientX: canvasPosition.x, clientY: canvasPosition.y, button: 0 } as MouseEvent;
         service.onMouseDown(mouseEvent);
@@ -335,5 +248,148 @@ describe('RectangleService', () => {
         expect(imageData.data[2]).not.toEqual(0); // B
         // tslint:disable-next-line:no-magic-numbers
         expect(imageData.data[3]).not.toEqual(0); // A
+    });
+
+    it('when tool is deselected, it should finalize the shape', () => {
+        service.onToolDeselect();
+        expect(finalizeSpy).toHaveBeenCalled();
+    });
+
+    it('when tool is deselected, it should unlock the history service', () => {
+        history.isLocked = true;
+        service.onToolDeselect();
+        expect(history.isLocked).toBeFalse();
+    });
+
+    it('when finalizing filled shape, registered user action should contain a fill renderer', () => {
+        service.shapeType = ShapeType.Filled;
+
+        service['mouseDown'] = true;
+        service.onMouseUp({} as MouseEvent);
+
+        const generatedUserAction = history['past'][0] as UserActionRenderShape;
+
+        const isGeneratedRendererOfTypeFill =
+            generatedUserAction['renderers'].find((renderer) => typeof renderer === typeof service['fillRenderer']) != undefined;
+
+        expect(isGeneratedRendererOfTypeFill).toBeTrue();
+    });
+
+    it('when finalizing contoured and filled shape, registered user action should contain a fill renderer', () => {
+        service.shapeType = ShapeType.ContouredAndFilled;
+
+        service['mouseDown'] = true;
+        service.onMouseUp({} as MouseEvent);
+
+        const generatedUserAction = history['past'][0] as UserActionRenderShape;
+
+        const isGeneratedRendererOfTypeFill =
+            generatedUserAction['renderers'].find((renderer) => typeof renderer === typeof service['fillRenderer']) != undefined;
+
+        expect(isGeneratedRendererOfTypeFill).toBeTrue();
+    });
+
+    it('when finalizing contoured shape, registered user action should contain a contour renderer', () => {
+        service.shapeType = ShapeType.Contoured;
+
+        service['mouseDown'] = true;
+        service.onMouseUp({} as MouseEvent);
+
+        const generatedUserAction = history['past'][0] as UserActionRenderShape;
+
+        const isGeneratedRendererOfTypeStroke =
+            generatedUserAction['renderers'].find((renderer) => typeof renderer === typeof service['strokeRenderer']) != undefined;
+
+        expect(isGeneratedRendererOfTypeStroke).toBeTrue();
+    });
+
+    it('when finalizing contoured and filled shape, registered user action should contain a contour renderer', () => {
+        service.shapeType = ShapeType.ContouredAndFilled;
+
+        service['mouseDown'] = true;
+        service.onMouseUp({} as MouseEvent);
+
+        const generatedUserAction = history['past'][0] as UserActionRenderShape;
+
+        const isGeneratedRendererOfTypeStroke =
+            generatedUserAction['renderers'].find((renderer) => typeof renderer === typeof service['strokeRenderer']) != undefined;
+
+        expect(isGeneratedRendererOfTypeStroke).toBeTrue();
+    });
+
+    it('when shape is drawn and type is filled, fillRenderer should be invoked', () => {
+        service.shapeType = ShapeType.Filled;
+
+        service['drawRect'].call(service, baseCtxStub);
+
+        expect(fillRenderSpy).toHaveBeenCalled();
+    });
+
+    it('when shape is drawn and type is contoured and filled, fillRenderer should be invoked', () => {
+        service.shapeType = ShapeType.ContouredAndFilled;
+
+        service['drawRect'].call(service, baseCtxStub);
+
+        expect(fillRenderSpy).toHaveBeenCalled();
+    });
+
+    it('when shape is drawn and type is contoured, strokeRenderer should be invoked', () => {
+        service.shapeType = ShapeType.Contoured;
+
+        service['drawRect'].call(service, baseCtxStub);
+
+        expect(strokeRenderSpy).toHaveBeenCalled();
+    });
+
+    it('when shape is drawn and type is contoured and filled, strokeRenderer should be invoked', () => {
+        service.shapeType = ShapeType.ContouredAndFilled;
+
+        service['drawRect'].call(service, baseCtxStub);
+
+        expect(strokeRenderSpy).toHaveBeenCalled();
+    });
+
+    it('on shift down, if mouse is down, shape should be drawn', () => {
+        service.mouseDown = true;
+        service.onKeyDown({ key: 'Shift' } as KeyboardEvent);
+        expect(drawRectSpy).toHaveBeenCalled();
+    });
+
+    it('on shift down, if mouse is up, shape should not be drawn', () => {
+        service.mouseDown = false;
+        service.onKeyDown({ key: 'Shift' } as KeyboardEvent);
+        expect(drawRectSpy).not.toHaveBeenCalled();
+    });
+
+    it('on shift up, if mouse is down, shape should be drawn', () => {
+        service.mouseDown = true;
+        service.onKeyUp({ key: 'Shift' } as KeyboardEvent);
+        expect(drawRectSpy).toHaveBeenCalled();
+    });
+
+    it('on shift up, if mouse is up, shape should not be drawn', () => {
+        service.mouseDown = false;
+        service.onKeyUp({ key: 'Shift' } as KeyboardEvent);
+        expect(drawRectSpy).not.toHaveBeenCalled();
+    });
+
+    it('when primary colour changes, so should fill style', () => {
+        const COLOUR = Colour.hexToRgb('424242');
+
+        colourService.setPrimaryColour(COLOUR);
+
+        colourService.primaryColourChanged.subscribe(() => {
+            expect(service['fillStyleProperty'].colour).toEqual(COLOUR);
+        });
+    });
+
+    it('when secondary colour changes, so should stroke style', () => {
+        const COLOUR = Colour.hexToRgb('424242');
+
+        colourService.setSecondaryColour(COLOUR);
+
+        colourService.secondaryColourChanged.subscribe(() => {
+            expect(service['strokeStyleProperty'].colour).toEqual(COLOUR);
+        });
     });
 });
