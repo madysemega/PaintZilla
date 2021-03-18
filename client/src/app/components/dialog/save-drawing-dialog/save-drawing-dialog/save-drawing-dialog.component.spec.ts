97357c48e0da8beedca82e508889ec353cf759c0
Emile Dumas <emile.dumas@polymtl.ca>
2021-03-17 23:44:49 -0400

    first test

--- client/src/app/components/dialog/save-drawing-dialog/save-drawing-dialog/save-drawing-dialog.component.spec.ts
+++ client/src/app/components/dialog/save-drawing-dialog/save-drawing-dialog/save-drawing-dialog.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ServerService } from '@app/commons/service/server.service';
 import { DrawingService } from '@app/drawing/services/drawing-service/drawing.service';
 import { HistoryService } from '@app/history/service/history.service';
 import { SaveDrawingDialogComponent } from './save-drawing-dialog.component';
@@ -10,6 +13,7 @@ fdescribe('SaveDrawingDialogComponent', () => {
 
     let historyServiceStub: HistoryService;
     let drawingServiceSpy: DrawingService;
+    let serverServiceSpy: ServerService;
 
     // tslint:disable:no-any
     let matDialogRefSpy: jasmine.SpyObj<any>;
@@ -17,13 +21,17 @@ fdescribe('SaveDrawingDialogComponent', () => {
     beforeEach(async(() => {
         historyServiceStub = new HistoryService();
         drawingServiceSpy = new DrawingService(historyServiceStub);
+        serverServiceSpy = TestBed.inject(ServerService);
         matDialogRefSpy = jasmine.createSpyObj('MatDialogRef<ExportDrawingDialogComponent>', ['close']);
         TestBed.configureTestingModule({
             declarations: [SaveDrawingDialogComponent],
             imports: [MatDialogModule],
             providers: [
                 { provide: MatDialogRef, useValue: matDialogRefSpy },
+                { provide: MatSnackBar, useValue: {} },
                 { provide: DrawingService, useValue: drawingServiceSpy },
+                { provide: ServerService, useValue: serverServiceSpy },
+                { provide: HttpClient, useValue: {} },
             ],
         }).compileComponents();
     }));
