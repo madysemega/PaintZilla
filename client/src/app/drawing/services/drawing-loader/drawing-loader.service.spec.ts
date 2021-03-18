a870a21b4f28bcdea06a81b71e59bf6a4e1eec07
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-03-18 00:45:35 -0400

    Created drawing loader service

---
+++ client/src/app/drawing/services/drawing-loader/drawing-loader.service.spec.ts
@@ -0,0 +1,32 @@
+import { TestBed } from '@angular/core/testing';
+import { DrawingService } from '../drawing-service/drawing.service';
+import { DrawingLoaderService } from './drawing-loader.service';
+
+
+describe('DrawingLoaderService', () => {
+    let service: DrawingLoaderService;
+
+    let drawingServiceSpy: jasmine.SpyObj<DrawingService>;
+
+    beforeEach(() => {
+        drawingServiceSpy = jasmine.createSpyObj('DrawingService', ['setImageFromBase64']);
+
+        TestBed.configureTestingModule({
+            providers: [
+                { provide: DrawingService, useValue: drawingServiceSpy },
+            ]
+        });
+        service = TestBed.inject(DrawingLoaderService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it("loadFromServer() should set the drawing surface's image to the downloaded base64 image", () => {
+        const IMAGE_ID = '1234567890';
+
+        service.loadFromServer(IMAGE_ID);
+        expect(drawingServiceSpy.setImageFromBase64).toHaveBeenCalled();
+    });
+});
