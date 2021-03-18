b08badbe2e10b3ffc03482a4383328aea469c3ff
madysemega <58781851+madysemega@users.noreply.github.com>
2021-03-18 08:43:37 -0400

    Added tests for database service

--- server/app/services/database/database.service.spec.ts
+++ server/app/services/database/database.service.spec.ts
@@ -1,14 +1,46 @@
-import { fail } from 'assert';
+import "reflect-metadata";
+// import { fail } from 'assert';
 import * as chai from 'chai';
 import * as mocha from 'mocha';
-import * as mongoose from 'mongoose';
+import * as chaiAsPromised from 'chai-as-promised';
 import { DatabaseService } from './database.service';
 import { MongoMemoryServer } from 'mongodb-memory-server';
-export class MockLocalDb {}
+import { LocalDatabaseService } from '@app/services/localDatabase/local.database.service';
+chai.use(chaiAsPromised);
+export const READY_STATE_DISCONNECTED = 0;
+export const READY_STATE_CONNECTED = 1;
 
 mocha.describe('Database service', () => {
     let databaseService: DatabaseService;
+    let mongoServer: MongoMemoryServer;
+    let mongoUri: string;
     beforeEach(async () => {
-        databaseService = new DatabaseService({} as MockLocalDb);
-    })
+        databaseService = new DatabaseService({} as LocalDatabaseService);
+        mongoServer = new MongoMemoryServer();
+        mongoUri = await mongoServer.getUri();
+    });
+    afterEach(async () => {
+        if(databaseService.distantDatabase.connection.readyState) {
+            await databaseService.closeConnection();
+        }
+    });
+
+    it ('start(): should connect to the database when start is called', async () => {
+        await databaseService.start(mongoUri);
+        chai.expect(databaseService.distantDatabase.connection.readyState).to.equal(READY_STATE_CONNECTED);
+    });
+
+    it ('start(): should not connect to the database when start is called with wrong URL', async () => {
+        try {
+            await databaseService.start('WRONG URL');
+        } catch {
+            chai.expect(databaseService.distantDatabase.connection.readyState).to.equal(READY_STATE_DISCONNECTED);
+        }
+    });
+
+    it ('closeConnection(): should no longer be connected if closeConnection is called', async () => {
+        await databaseService.start(mongoUri);
+        await databaseService.closeConnection();
+        chai.expect(databaseService.distantDatabase.connection.readyState).to.equal(READY_STATE_DISCONNECTED);
+    });
 });
