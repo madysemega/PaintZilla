3ee1e66544bc78dd1c5453da5665cfc5c1c670b7
madysemega <58781851+madysemega@users.noreply.github.com>
2021-03-18 07:02:27 -0400

    Added mongodb memory server for testing

--- server/app/services/database/database.service.spec.ts
+++ server/app/services/database/database.service.spec.ts
@@ -3,11 +3,12 @@ import * as chai from 'chai';
 import * as mocha from 'mocha';
 import * as mongoose from 'mongoose';
 import { DatabaseService } from './database.service';
-
+import { MongoMemoryServer } from 'mongodb-memory-server';
+export class MockLocalDb {}
 
 mocha.describe('Database service', () => {
     let databaseService: DatabaseService;
     beforeEach(async () => {
-        databaseService = new DatabaseService();
+        databaseService = new DatabaseService({} as MockLocalDb);
     })
 });
