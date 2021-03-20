a8aad0b844e50545c693d97273c0285d9ea3e931
madysemega <58781851+madysemega@users.noreply.github.com>
2021-03-20 10:27:15 -0400

    Allowed special characters '_@./#'&+' and space in labels name

--- common/validation/regular.expressions.ts
+++ common/validation/regular.expressions.ts
@@ -5,4 +5,4 @@ export const BASE_64_REGEX = /(data:image\/[^;]+;base64,[^"]+)/;
 // ref: https://stackoverflow.com/questions/13283470/regex-for-allowing-alphanumeric-and-space
 export const NAME_REGEX = /^[a-zA-Z0-9\d\-_\s]+$/i;
 // ref: https://stackoverflow.com/questions/14639378/regex-to-validate-only-alphanumeric-no-spaces-and-only-40-characters
-export const LABEL_REGEX = /^[a-zA-Z0-9\-_]{1,15}$/;
+export const LABEL_REGEX = /^[A-Za-z0-9_@./#'&+\d\-_\s]{1,15}$/;
