618e002d41fac7a63c3e2c891d954004a92632a0
madysemega <58781851+madysemega@users.noreply.github.com>
2021-03-23 07:01:42 -0400

    Added colour picker service tests

--- client/src/app/colour-picker/services/colour-picker/colour-picker.service.spec.ts
+++ client/src/app/colour-picker/services/colour-picker/colour-picker.service.spec.ts
@@ -1,9 +1,10 @@
-import { ColourPickerService } from '@app/colour-picker/services/colour-picker/colour-picker.service';
-import { Colour } from '@app/colour-picker/classes/colours.class';
 import { TestBed } from '@angular/core/testing';
+import { Colour } from '@app/colour-picker/classes/colours.class';
+import { ColourPickerService } from '@app/colour-picker/services/colour-picker/colour-picker.service';
 export const DEFAULT_COLOUR = new Colour();
+export const TEST_COLOUR = new Colour(1, 1, 1, 1);
 // tslint:disable: no-string-literal
-fdescribe('ColourPickerService', () => {
+describe('ColourPickerService', () => {
     let service: ColourPickerService;
     beforeEach(() => {
         spyOn(Colour, 'hsvToRgb').and.returnValue(DEFAULT_COLOUR);
@@ -20,5 +21,65 @@ fdescribe('ColourPickerService', () => {
     it('getHue(): should return hueSubject.value', () => {
         const expected = 10;
         service['hueSubject'].next(expected);
+        expect(service.getHue()).toEqual(expected);
+    });
+
+    it('getSaturation(): should return hueSubject.value', () => {
+        const expected = 10;
+        service['saturationSubject'].next(expected);
+        expect(service.getSaturation()).toEqual(expected);
+    });
+
+    it('getValue(): should return hueSubject.value', () => {
+        const expected = 10;
+        service['valueSubject'].next(expected);
+        expect(service.getValue()).toEqual(expected);
+    });
+
+    it('getAlpha(): should return hueSubject.value', () => {
+        const expected = 10;
+        service['alphaSubject'].next(expected);
+        expect(service.getAlpha()).toEqual(expected);
+    });
+
+    it('set hue: should set hue', () => {
+        const expected = 10;
+        service.hue = expected;
+        expect(service.getHue()).toEqual(expected);
+    });
+
+    it('set saturation: should set saturation', () => {
+        const expected = 10;
+        service.saturation = expected;
+        expect(service.getSaturation()).toEqual(expected);
+    });
+
+    it('set value: should set value', () => {
+        const expected = 10;
+        service.value = expected;
+        expect(service.getValue()).toEqual(expected);
+    });
+
+    it('set alpha: should set alpha', () => {
+        const expected = 10;
+        service.alpha = expected;
+        expect(service.getAlpha()).toEqual(expected);
+    });
+
+    it('getCurrentColor(): should return currentColour', () => {
+        service['currentColour'] = DEFAULT_COLOUR;
+        expect(service.getCurrentColor()).toEqual(DEFAULT_COLOUR);
+    });
+
+    it('setCurrentColour(): should set currentColour to specified value if they are not equal', () => {
+        service['currentColour'] = TEST_COLOUR;
+        service.setCurrentColour(DEFAULT_COLOUR);
+        expect(service.getCurrentColor()).toEqual(DEFAULT_COLOUR);
+    });
+
+    it('setCurrentColour(): should not set currentColour to specified value if they are equal', () => {
+        service['currentColour'] = DEFAULT_COLOUR;
+        service.setCurrentColour(DEFAULT_COLOUR);
+        expect(service.getCurrentColor()).toEqual(DEFAULT_COLOUR);
     });
 });
