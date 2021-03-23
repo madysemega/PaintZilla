75f51ccd2267857b14f1b2247eb00bcd524d0019
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-03-23 00:31:41 -0400

    Carousel drawings now always stay the same size, but ajust to keep aspect ratio

--- client/src/app/carousel/components/image-details/image-details.component.ts
+++ client/src/app/carousel/components/image-details/image-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import { Vec2 } from '@app/app/classes/vec2';
 import { Drawing } from '@common/models/drawing';
 
 @Component({
@@ -9,6 +10,27 @@ import { Drawing } from '@common/models/drawing';
     styleUrls: ['./image-details.component.scss'],
 })
 export class ImageDetailsComponent {
+    get imageSrc(): SafeResourceUrl {
+        return this.domSanitizer.bypassSecurityTrustResourceUrl(this.data.drawing);
+    }
+
+    get imageWidth(): number {
+        const imageDimensions = this.getRealImageDimensions();
+        const isWidthGreaterThanHeight = imageDimensions.x > imageDimensions.y;
+
+        return isWidthGreaterThanHeight ? this.imageContainerWidth : (this.imageContainerHeight / imageDimensions.y) * imageDimensions.x;
+    }
+
+    get imageHeight(): number {
+        const imageDimensions = this.getRealImageDimensions();
+        const isHeightGreaterThanWidth = imageDimensions.x < imageDimensions.y;
+
+        return isHeightGreaterThanWidth ? this.imageContainerHeight : (this.imageContainerWidth / imageDimensions.x) * imageDimensions.y;
+    }
+
+    constructor(private domSanitizer: DomSanitizer, private router: Router) {
+        this.delete = new EventEmitter();
+    }
     @Input() data: Drawing = {
         id: '',
         name: '',
@@ -18,13 +40,8 @@ export class ImageDetailsComponent {
 
     @Output() delete: EventEmitter<string>;
 
-    constructor(private domSanitizer: DomSanitizer, private router: Router) {
-        this.delete = new EventEmitter();
-    }
-
-    get imageSrc(): SafeResourceUrl {
-        return this.domSanitizer.bypassSecurityTrustResourceUrl(this.data.drawing);
-    }
+    imageContainerWidth: number = 150;
+    imageContainerHeight: number = 150;
 
     loadImage(): void {
         this.router.navigate([`/editor/${this.data.id}`]);
@@ -34,5 +51,12 @@ export class ImageDetailsComponent {
         event.preventDefault();
         event.stopPropagation();
         this.delete.emit(this.data.id);
+    }
+
+    private getRealImageDimensions(): Vec2 {
+        const image = new Image();
+        image.src = this.data.drawing;
+
+        return { x: image.width, y: image.height };
     }
 }
