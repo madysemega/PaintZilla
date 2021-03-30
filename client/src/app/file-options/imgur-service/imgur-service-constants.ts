ada511655220b513f175db9b9291c1e1f003be81
Emile Dumas <emile.dumas@polymtl.ca>
2021-03-29 20:25:04 -0400

    added tests for imgur-service

--- client/src/app/file-options/imgur-service/imgur-service-constants.ts
+++ client/src/app/file-options/imgur-service/imgur-service-constants.ts
@@ -2,7 +2,15 @@ import { HttpHeaders } from '@angular/common/http';
 
 export const HEADER_OPTIONS = {
     headers: new HttpHeaders({
-        Authorization: 'Client-ID 38e085579dc56b9',
+        // tslint:disable:quotemark
+        // tslint:disable:prettier
+        "Authorization": 'Client-ID 38e085579dc56b9',
         'Content-Type': 'application/json',
     }),
 };
+
+export interface ImgurRequest {
+    image: string;
+    type: string;
+    data: { link: string };
+}
