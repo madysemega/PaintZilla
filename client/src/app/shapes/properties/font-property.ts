1281cf4ec1fbab42abd26e0b2cf9fd2f18ff141b
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-03-31 11:56:46 -0400

    Added font property to shape properties

---
+++ client/src/app/shapes/properties/font-property.ts
@@ -0,0 +1,15 @@
+import { ShapeProperty } from './shape-property';
+
+export class FontProperty extends ShapeProperty {
+    constructor(public fontSize: number) {
+        super();
+    }
+
+    apply(ctx: CanvasRenderingContext2D): void {
+        ctx.font = `${this.fontSize}px`;
+    }
+
+    clone(): ShapeProperty {
+        return new FontProperty(this.fontSize);
+    }
+}
