1714e4227e359d0da300ab4e8fbe3990b957fca6
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-04-03 15:05:08 -0400

    Added method for font size update event in text tool configuration component

--- client/src/app/tools/components/tool-configurations/text-tool-configuration/text-tool-configuration.component.spec.ts
+++ client/src/app/tools/components/tool-configurations/text-tool-configuration/text-tool-configuration.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatSelectChange } from '@angular/material/select';
+import { TextService } from '@app/tools/services/tools/text/text.service';
 import { HotkeyModule, HotkeysService } from 'angular2-hotkeys';
 import { TextToolConfigurationComponent } from './text-tool-configuration.component';
 
@@ -28,5 +30,14 @@ describe('TextToolConfigurationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('When font size is updated, event should be propagated to service', () => {
+    const SERVICE_METHOD_SPY = spyOn(TestBed.inject(TextService), 'updateFontSize').and.stub();
+    const NEW_FONT_SIZE = 68;
+    const FONT_SIZE_CHANGE_EVENT = { value: NEW_FONT_SIZE } as MatSelectChange;
+
+    component.updateFontSize(FONT_SIZE_CHANGE_EVENT);
+    expect(SERVICE_METHOD_SPY).toHaveBeenCalledWith(NEW_FONT_SIZE);
   });
 });
