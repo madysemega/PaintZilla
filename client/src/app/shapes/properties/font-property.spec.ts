53f38bf3cd54aa68b97f1539891352bf0fe6ca05
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-04-03 14:08:29 -0400

    Added tests for font property

---
+++ client/src/app/shapes/properties/font-property.spec.ts
@@ -0,0 +1,28 @@
+import { CanvasTestHelper } from '@app/app/classes/canvas-test-helper';
+import { FontProperty } from './font-property';
+
+describe('FontProperty', () => {
+    let property: FontProperty;
+
+    const FONT_SIZE = 32;
+
+    let canvasTestHelper: CanvasTestHelper;
+    let ctxStub: CanvasRenderingContext2D;
+
+    beforeEach(() => {
+        property = new FontProperty(FONT_SIZE);
+
+        canvasTestHelper = new CanvasTestHelper();
+        ctxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+    });
+
+    it("when apply() is called, it should set the canvas rendering context's font property to the font specified upon instantiation", () => {
+        property.apply(ctxStub);
+        expect(ctxStub.font).toEqual(`${FONT_SIZE}px serif`);
+    });
+
+    it('clone should return an identical copy', () => {
+        const clone = property.clone() as FontProperty;
+        expect(clone.fontSize).toEqual(property.fontSize);
+    });
+});
