6e1f2b60caebbd9b5544e147b1af59598d191033
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-04-06 13:13:10 -0400

    Added keyboard shortcut to toggle magnetism

--- client/src/app/magnetism/magnetism.service.ts
+++ client/src/app/magnetism/magnetism.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { KeyboardService } from '@app/keyboard/keyboard.service';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class MagnetismService {
+    constructor(private keyboardService: KeyboardService) {
+        this.registerKeyboardShortcuts();
+    }
+
     isActivated: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+    private registerKeyboardShortcuts(): void {
+        this.keyboardService.registerAction({
+            trigger: 'm',
+            invoke: () => this.toggleMagnetism(),
+            uniqueName: 'Toggle magnetism',
+            contexts: ['editor'],
+        });
+    }
 
     toggleMagnetism(): void {
         this.isActivated.next(!this.isActivated.value);
