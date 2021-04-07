851528816778c6d24ed4d5f5316bb4206850edd2
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-04-06 23:19:59 -0400

    Implemented bold option for text tool

--- client/src/app/shapes/properties/font-property.ts
+++ client/src/app/shapes/properties/font-property.ts
@@ -1,15 +1,15 @@
 import { ShapeProperty } from './shape-property';
 
 export class FontProperty extends ShapeProperty {
-    constructor(public fontSize: number, public fontName: string) {
+    constructor(public fontSize: number, public fontName: string, public isBold: boolean) {
         super();
     }
 
     apply(ctx: CanvasRenderingContext2D): void {
-        ctx.font = `${this.fontSize}px ${this.fontName}`;
+        ctx.font = `${this.isBold ? 'bold' : ''} ${this.fontSize}px ${this.fontName}`;
     }
 
     clone(): ShapeProperty {
-        return new FontProperty(this.fontSize, this.fontName);
+        return new FontProperty(this.fontSize, this.fontName, this.isBold);
     }
 }
