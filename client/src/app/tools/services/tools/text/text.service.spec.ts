6e6deb961ee40e62445840efdf5e209bc54aa867
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-04-07 11:52:07 -0400

    Added tests for cancelling the text tool with escape

--- client/src/app/tools/services/tools/text/text.service.spec.ts
+++ client/src/app/tools/services/tools/text/text.service.spec.ts
@@ -109,6 +109,28 @@ describe('TextService', () => {
         expect(clearCanvasSpy).toHaveBeenCalled();
     });
 
+    it('Cancelling the shape should disable the cursor', () => {
+        const editorDisableCursorSpy = spyOn(service['editor'], 'disableCursor').and.stub();
+        spyOn(TestBed.inject(DrawingService), 'clearCanvas').and.stub();
+        service['cancel']();
+        expect(editorDisableCursorSpy).toHaveBeenCalled();
+    });
+
+    it('Cancelling the shape should clear canvas', () => {
+        spyOn(service['editor'], 'disableCursor').and.stub();
+        const clearCanvasSpy = spyOn(TestBed.inject(DrawingService), 'clearCanvas').and.stub();
+        service['cancel']();
+        expect(clearCanvasSpy).toHaveBeenCalled();
+    });
+
+    it('Cancelling the shape should reset the service', () => {
+        spyOn(service['editor'], 'disableCursor').and.stub();
+        spyOn(TestBed.inject(DrawingService), 'clearCanvas').and.stub();
+        const resetSpy = spyOn<any>(service, 'reset').and.stub();
+        service['cancel']();
+        expect(resetSpy).toHaveBeenCalled();
+    });
+
     it('If editor is not empty, a user action should be registered to the history service upon finalization', () => {
         spyOn(service['editor']['renderer'], 'render').and.stub();
         spyOn(service['editor']['cursorRenderer'], 'render').and.stub();
@@ -252,6 +274,15 @@ describe('TextService', () => {
         service['isEditing'] = true;
         service.onKeyUp(keyboardEvent);
         expect(deleteSpy).toHaveBeenCalled();
+    });
+
+    it('If editing, delete key should remove character to the right of the cursor in the text', () => {
+        const keyboardEvent = new KeyboardEvent('onKeyUp', { key: 'Escape' });
+        const cancelSpy = spyOn<any>(service, 'cancel').and.stub();
+
+        service['isEditing'] = true;
+        service.onKeyUp(keyboardEvent);
+        expect(cancelSpy).toHaveBeenCalled();
     });
 
     it('If editing, enter key should write \\n to the text', () => {
