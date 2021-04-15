543d7ffee8fe0a061e7b19796085838ae2aa9f20
Felix Duguay <felixduguay99@gmail.com>
2021-04-15 15:00:38 -0400

    Commit avec lint

--- client/src/app/tools/components/tool-configurations/spray-tool-configuration/spray-tool-configuration.component.constant.ts
+++ client/src/app/tools/components/tool-configurations/spray-tool-configuration/spray-tool-configuration.component.constant.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_DROP_DIAMETER = 5;
 export const DEFAULT_JET_DIAMETER = 50;
 export const DEFAULT_NB_DROPS_PER_SECONDS = 50;
+// tslint:disable:no-magic-numbers
+export const SAMPLE_DIAMETERS = [5, 1, 52, 42];
