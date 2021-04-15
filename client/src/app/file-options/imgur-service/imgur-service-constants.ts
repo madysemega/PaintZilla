22160fe583a12070f078ea35bd3643d8658ce78d
Emile Dumas <emile.dumas@polymtl.ca>
2021-04-15 14:59:34 -0400

    moved interface into a separate file

--- client/src/app/file-options/imgur-service/imgur-service-constants.ts
+++ client/src/app/file-options/imgur-service/imgur-service-constants.ts
@@ -4,13 +4,7 @@ export const HEADER_OPTIONS = {
     headers: new HttpHeaders({
         // tslint:disable:quotemark
         // tslint:disable:prettier
-        "Authorization": 'Client-ID 38e085579dc56b9',
+        Authorization: 'Client-ID 38e085579dc56b9',
         'Content-Type': 'application/json',
     }),
 };
-
-export interface ImgurRequest {
-    image: string;
-    type: string;
-    data: { link: string };
-}
