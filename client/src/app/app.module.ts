0830ca2f2d728dd1800e776ed07a6d6429e65f4b
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-04-15 17:36:54 -0400

    Added lasso selection component to app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -44,6 +44,7 @@ import { FilterLabelComponent } from './carousel/components/filter-label/filter-
 import { ConfigurationPanelComponent } from './drawing/components/configuration-panel/configuration-panel.component';
 import { MagnetismComponent } from './magnetism/magnetism/magnetism.component';
 import { ClipboardComponent } from './tools/components/selection/clipboard/clipboard.component';
+import { LassoSelectionComponent } from './tools/components/selection/lasso-selection/lasso-selection.component';
 import { StampComponent } from './tools/components/tool-configurations/stamp/stamp.component';
 @NgModule({
     declarations: [
@@ -89,6 +90,7 @@ import { StampComponent } from './tools/components/tool-configurations/stamp/sta
         ImgurLinkDialogComponent,
         StampComponent,
         TextToolConfigurationComponent,
+        LassoSelectionComponent,
     ],
     imports: [
         BrowserModule,
