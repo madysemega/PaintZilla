8bb9328f67880c9c4807efa04ea702b6a2cc0c61
Emile Dumas <emile.dumas@polymtl.ca>
2021-04-16 11:24:41 -0400

    fixed negative boolean expression

--- client/src/app/drawing/services/drawing-creator/drawing-creator.service.ts
+++ client/src/app/drawing/services/drawing-creator/drawing-creator.service.ts
@@ -19,7 +19,7 @@ export class DrawingCreatorService {
     }
 
     createNewDrawing(): void {
-        if (this.drawingService.isCanvasEmpty() || !this.noDialogsOpen()) return;
+        if (this.drawingService.isCanvasEmpty() || !this.dialogsAreOpen()) return;
         this.dialogRef = this.dialog.open(DiscardChangesDialogComponent, { disableClose: true, panelClass: 'custom-modalbox' });
         this.dialogRef.afterClosed().subscribe((result) => {
             if (result === 'discard') this.clearCanvasAndActions();
@@ -32,7 +32,7 @@ export class DrawingCreatorService {
         });
     }
 
-    noDialogsOpen(): boolean {
+    dialogsAreOpen(): boolean {
         return this.dialog.openDialogs.length === 0;
     }
 
