75249499642d3dd9d3782f1495a2f3f65097164d
Charles <charles.fakih@polymtl.ca>
2021-04-17 13:18:55 -0400

    Refactored polygon

--- client/src/app/tools/services/tools/polygon/polygon.service.ts
+++ client/src/app/tools/services/tools/polygon/polygon.service.ts
@@ -33,17 +33,18 @@ export class PolygonService extends ShapeTool implements ISelectableTool {
     private shape: PolygonShape = new PolygonShape({ x: 0, y: 0 }, { x: 0, y: 0 }, Constants.TRIANGLE_SIDES, this.lineWidth);
 
     strokeWidthProperty: StrokeWidthProperty = new StrokeWidthProperty(this.lineWidth);
-    colourProperty: StrokeStyleProperty = new StrokeStyleProperty(this.colourService.getSecondaryColour());
-    strokeStyleProperty: StrokeStyleProperty = new FillStyleProperty(this.colourService.getPrimaryColour());
+    strokeStyleProperty: StrokeStyleProperty = new StrokeStyleProperty(this.colourService.getSecondaryColour());
     fillStyleProperty: FillStyleProperty = new FillStyleProperty(this.colourService.getPrimaryColour());
-
     private strokeRenderer: PolygonStrokeRenderer = new PolygonStrokeRenderer(this.shape, [this.strokeWidthProperty, this.strokeStyleProperty]);
     private fillRenderer: PolygonFillRenderer = new PolygonFillRenderer(this.shape, [this.fillStyleProperty]);
-
     constructor(drawingService: DrawingService, private colourService: ColourService, private history: HistoryService) {
         super(drawingService);
         this.shapeType = ShapeType.Contoured;
         this.key = 'polygon';
+        this.initializeProperties();
+    }
+
+    private initializeProperties(): void {
         this.colourService.secondaryColourChanged.subscribe((colour: Colour) => (this.strokeStyleProperty.colour = colour));
         this.colourService.primaryColourChanged.subscribe((colour: Colour) => (this.fillStyleProperty.colour = colour));
     }
@@ -129,52 +130,64 @@ export class PolygonService extends ShapeTool implements ISelectableTool {
     drawPolygon(ctx: CanvasRenderingContext2D, startPoint: Vec2, endPoint: Vec2): void {
         this.drawingService.clearCanvas(this.drawingService.previewCtx);
 
-        const shouldRenderStroke = this.shapeType === ShapeType.Contoured || this.shapeType === ShapeType.ContouredAndFilled;
-        const shouldRenderFill = this.shapeType === ShapeType.Filled || this.shapeType === ShapeType.ContouredAndFilled;
-
         endPoint = this.getSquareEndPoint(startPoint, endPoint);
 
         const CENTER_POINT: Vec2 = { x: (startPoint.x + endPoint.x) / 2, y: (startPoint.y + endPoint.y) / 2 };
-        const HALF_STROKE_WIDTH = this.strokeWidthProperty.strokeWidth / 2;
+        this.setPolygonAttributes(startPoint, endPoint);
 
-        this.shape.topLeft.x = startPoint.x + (shouldRenderStroke ? HALF_STROKE_WIDTH : 0);
-        this.shape.topLeft.y = startPoint.y + (shouldRenderStroke ? HALF_STROKE_WIDTH : 0);
-        this.shape.bottomRight.x = endPoint.x - (shouldRenderStroke ? HALF_STROKE_WIDTH : 0);
-        this.shape.bottomRight.y = endPoint.y - (shouldRenderStroke ? HALF_STROKE_WIDTH : 0);
-        this.shape.numberSides = this.numberSides;
+        this.renderPolygon(ctx);
 
         const SIZE = this.squarePoint(CENTER_POINT, this.shape.bottomRight);
+        this.drawPerimeter(ctx, CENTER_POINT, Math.abs(SIZE));
+    }
 
-        if (shouldRenderFill) {
+    renderPolygon(ctx: CanvasRenderingContext2D): void {
+        const SHOULD_RENDER_STROKE = this.shapeType === ShapeType.Contoured || this.shapeType === ShapeType.ContouredAndFilled;
+        const SHOULD_RENDER_FILL = this.shapeType === ShapeType.Filled || this.shapeType === ShapeType.ContouredAndFilled;
+        if (SHOULD_RENDER_FILL) {
             this.fillRenderer.render(ctx);
         }
-        if (shouldRenderStroke) {
+        if (SHOULD_RENDER_STROKE) {
             this.strokeRenderer.render(ctx);
-        }
-        if (this.isToDrawPerim) {
-            this.drawPerimeter(ctx, CENTER_POINT, Math.abs(SIZE));
         }
     }
 
+    setPolygonAttributes(startPoint: Vec2, endPoint: Vec2): void {
+        const SHOULD_RENDER_STROKE = this.shapeType === ShapeType.Contoured || this.shapeType === ShapeType.ContouredAndFilled;
+        const HALF_STROKE_WIDTH = this.strokeWidthProperty.strokeWidth / 2;
+
+        this.shape.topLeft.x = startPoint.x + (SHOULD_RENDER_STROKE ? HALF_STROKE_WIDTH : 0);
+        this.shape.topLeft.y = startPoint.y + (SHOULD_RENDER_STROKE ? HALF_STROKE_WIDTH : 0);
+        this.shape.bottomRight.x = endPoint.x - (SHOULD_RENDER_STROKE ? HALF_STROKE_WIDTH : 0);
+        this.shape.bottomRight.y = endPoint.y - (SHOULD_RENDER_STROKE ? HALF_STROKE_WIDTH : 0);
+        this.shape.numberSides = this.numberSides;
+    }
+
     drawPerimeter(ctx: CanvasRenderingContext2D, center: Vec2, size: number): void {
-        const shouldRenderFill = this.shapeType === ShapeType.Filled;
-        const DASH_NUMBER = 8;
+        if (!this.isToDrawPerim) return;
         ctx.save();
         ctx.beginPath();
-        ctx.setLineDash([DASH_NUMBER]);
-        ctx.strokeStyle = '#888';
-        ctx.lineWidth = 1;
-
-        const STROKE_DISTANCE =
-            this.lineWidth / Math.sin((Math.PI + Math.PI * (this.numberSides - Constants.TRIANGLE_SIDES)) / (2 * this.numberSides)) / 2;
-
-        if (shouldRenderFill && size > 0) size = Math.abs(size - this.lineWidth / 2);
-        else if (shouldRenderFill) size = Math.abs(size + this.lineWidth / 2);
-        else if (!shouldRenderFill && size > 0) size = Math.abs(size + STROKE_DISTANCE);
-        else size = Math.abs(size - STROKE_DISTANCE);
-
+        size = this.setPerimeterAttributes(ctx, size);
         ctx.ellipse(center.x, center.y, size, size, 0, 0, Constants.CIRCLE_MAX_ANGLE);
         ctx.stroke();
         ctx.restore();
+    }
+    setPerimeterAttributes(ctx: CanvasRenderingContext2D, size: number): number {
+        const DASH_NUMBER = 8;
+        ctx.setLineDash([DASH_NUMBER]);
+        ctx.strokeStyle = '#888';
+        ctx.lineWidth = 1;
+        let strokeDistance =
+            this.lineWidth / Math.sin((Math.PI + Math.PI * (this.numberSides - Constants.TRIANGLE_SIDES)) / (2 * this.numberSides)) / 2;
+
+        const SHOULD_RENDER_FILL = this.shapeType === ShapeType.Filled;
+        const SUB_HALF_LINE_WIDTH = SHOULD_RENDER_FILL && size > 0 ? 1 : 0;
+        const ADD_HALF_LINE_WIDTH = SHOULD_RENDER_FILL && size <= 0 ? 1 : 0;
+        const ADD_STROKE_DISTANCE = !SHOULD_RENDER_FILL && size > 0 ? 1 : 0;
+        const SUB_STROKE_DISTANCE = !SHOULD_RENDER_FILL && size <= 0 ? 1 : 0;
+        const HALF_LINE_WIDTH = (ADD_HALF_LINE_WIDTH * this.lineWidth) / 2 - (SUB_HALF_LINE_WIDTH * this.lineWidth) / 2;
+        strokeDistance = ADD_STROKE_DISTANCE * strokeDistance - SUB_STROKE_DISTANCE * strokeDistance;
+
+        return Math.abs(size + HALF_LINE_WIDTH + strokeDistance);
     }
 }
