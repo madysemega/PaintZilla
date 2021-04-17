0eb08964497ca1cdca196e4ae42a6920131972c3
NeoIsThere <samuel.sam.956@gmail.com>
2021-04-17 17:11:59 -0400

    provided value at initialization for handler memento

--- client/src/app/app/classes/handler-memento.ts
+++ client/src/app/app/classes/handler-memento.ts
@@ -1,40 +1,28 @@
 import { Vec2 } from './vec2';
 
 export class HandlerMemento {
+
+    selection: HTMLCanvasElement = document.createElement('canvas');
+    originalSelection: HTMLCanvasElement  = document.createElement('canvas');
+    selectionCtx: CanvasRenderingContext2D = this.selection.getContext('2d') as CanvasRenderingContext2D;
+    originalSelectionCtx: CanvasRenderingContext2D = this.originalSelection.getContext('2d') as CanvasRenderingContext2D;
+
+    topLeftRelativeToMiddle: Vec2 = { x: 0, y: 0 };
+    offset: Vec2 = { x: 0, y: 0 };
+    originalWidth: number =0;
+    originalHeight: number =0;
+    hasBeenManipulated: boolean = false;
+    needWhitePostDrawing: boolean = false;
+    originalTopLeftOnBaseCanvas: Vec2 = { x: 0, y: 0 };
+    originalCenter: Vec2 = { x: 0, y: 0 };
+    originalVertices: Vec2[] = [];
+    currentHorizontalScaling: number =1;
+    currentVerticalScaling: number =1;
+
     constructor(width: number, height: number) {
-        this.selection = document.createElement('canvas');
-        this.selectionCtx = this.selection.getContext('2d') as CanvasRenderingContext2D;
-
-        this.originalSelection = document.createElement('canvas');
-        this.originalSelectionCtx = this.originalSelection.getContext('2d') as CanvasRenderingContext2D;
-
         this.selection.width = width;
         this.selection.height = height;
         this.originalSelection.width = width;
         this.originalSelection.height = height;
-
-        this.topLeftRelativeToMiddle = { x: 0, y: 0 };
-        this.offset = { x: 0, y: 0 };
-        this.originalTopLeftOnBaseCanvas = { x: 0, y: 0 };
-        this.originalCenter = { x: 0, y: 0 };
-        this.originalVertices = [];
     }
-
-    selection: HTMLCanvasElement;
-    originalSelection: HTMLCanvasElement;
-
-    selectionCtx: CanvasRenderingContext2D;
-    originalSelectionCtx: CanvasRenderingContext2D;
-
-    topLeftRelativeToMiddle: Vec2;
-    offset: Vec2;
-    originalWidth: number;
-    originalHeight: number;
-    hasBeenManipulated: boolean;
-    needWhitePostDrawing: boolean;
-    originalTopLeftOnBaseCanvas: Vec2;
-    originalCenter: Vec2;
-    originalVertices: Vec2[];
-    currentHorizontalScaling: number;
-    currentVerticalScaling: number;
 }
