d6a11a67c12fdccfea18100c22dc1d81b9bba22c
Clément Gassmann-Prince <cfmgp@hotmail.com>
2021-04-18 21:16:02 -0400

    Added constants file for text tool service

---
+++ client/src/app/tools/services/tools/text/text-tool.constants.ts
@@ -0,0 +1,18 @@
+export const ALLOWED_CHAR_CLASSES: string[] = [
+    'Key',
+    'Digit',
+    'Numpad',
+    'Comma',
+    'Period',
+    'Quote',
+    'Backquote',
+    'Slash',
+    'Backslash',
+    'Bracket',
+    'Space',
+    'Minus',
+    'Equal',
+];
+
+export const DEFAULT_CURSOR_POSITION = 0;
+export const CURSOR_BLINK_DELAI_MS = 500;
